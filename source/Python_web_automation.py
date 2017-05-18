daec04876451af2310dab297c455f0e88655adb0
victor li <vili@nvidia.com>
2017-05-19 00:00:38 +0800

    nb 2017-05-19

--- source/Python_web_automation.py
+++ source/Python_web_automation.py
@@ -2,14 +2,145 @@
 
 Q_dict = { 
 ######
-#Q_0_0 #:adfadfafa: : : : 
+#Q_1_1 #:how to automate browsing using python
 ######
+"Q_1_1": ["how to automate browsing using python",
+    {
+    "url" :"http://stackoverflow.com/questions/1292817/how-to-automate-browsing-using-python",
+    "abstract" :"2009年8月18日 - suppose, I need to perform a set of procedure on a particular website ... You can also take a look at mechanize. Its meant to handle stateful ...",
+    "title" :"browser automation - How to automate browsing using python ...",
+    },
+    {
+    "url" :"https://automatetheboringstuff.com/chapter11/",
+    "abstract" :"You could take a few steps out of this task by writing a simple script to automatically launch the map in your browser using the contents of your clipboard.",
+    "title" :"Automate the Boring Stuff with Python",
+    },
+    {
+    "url" :"https://www.youtube.com/watch?v=dnlSRkifsOs",
+    "abstract" :"automated web browser login into Facebook automatically.",
+    "title" :"how to automate browsing using python - YouTube",
+    "metadata" :"2017年2月12日 - 上傳者：pro_tech",
+    },
+    {
+    "url" :"https://splinter.readthedocs.io/",
+    "abstract" :"Splinter is an open source tool for testing web applications using Python. It lets you automate browser actions, such as visiting URLs and interacting with their ...",
+    "title" :"Splinter — Splinter 0.7.5 documentation",
+    },
+    {
+    "url" :"https://www.reddit.com/r/Python/comments/1nk0r4/python_library_for_automating_interaction_with_a/",
+    "abstract" :"2013年10月2日 - Hi all, a python newb here, There's a web-tool I would like to use. ... from splinter import Browser with Browser() as browser: browser.visit(url) ...",
+    "title" :"Python library for automating interaction with a webpage? : Python",
+    },
+    {
+    "url" :"https://medium.com/the-python-corner/web-test-automation-in-python-a319a0783187",
+    "abstract" :"2016年11月15日 - Splinter is an open source tool for testing web applications using Python. It lets you automate browser actions, such as visiting URLs and ...",
+    "title" :"Web-Test Automation in Python – The Python corner – Medium",
+    },
+    {
+    "url" :"https://www.3pillarglobal.com/insights/automated-browser-performance-testing-using-python-and-autoit",
+    "abstract" :"3Pillar blog post on Automated Browser Performance Testing using Python and AutoIT. Performances compared between custom browser and native browser.",
+    "title" :"Automated Browser Performance Testing using Python and AutoIT",
+    },
+    {
+    "url" :"https://www.quora.com/Is-it-possible-to-write-a-Python-script-for-opening-a-browser-and-logging-into-a-website-How-could-you-do-it",
+    "abstract" :"Yes, it's definitely possible. Selenium is perfect for what you're trying to do. There's even a Python module that will let you integrate with browsers like Firefox and ...",
+    "title" :"Is it possible to write a Python script for opening a browser and logging ...",
+    },
+    {
+    "url" :"http://www.techbeamers.com/selenium-webdriver-python-tutorial/",
+    "abstract" :"Learn best practices to use Selenium Webdriver Python for web automation. Also ... It can interact with all types of Web browsers available till date like Firefox, ...",
+    "title" :"Selenium Webdriver Python Tutorial for Web Automation Testing",
+    },
+    {
+    "url" :"https://crondev.wordpress.com/2014/07/15/python-automate-navigation-through-websites-part-1/",
+    "abstract" :"2014年7月15日 - Mechanize is a very powerful engine and includes a browser itself. So users don't need to worry about cookies as long as they use the same ...",
+    "title" :"Python- automate navigation through websites (Part 1) – Cron-Dev",
+    },
+    ],
+######
+#Q_1_0 #:python  web automation
+######
+"Q_1_0": ["python  web automation",
+    {
+    "url" :"https://automatetheboringstuff.com/chapter11/",
+    "abstract" :"In this chapter, you will learn about several modules that make it easy to scrape web pages in Python. webbrowser . Comes with Python and opens a browser to ...",
+    "title" :"Web Scraping - Automate the Boring Stuff with Python",
+    },
+    {
+    "url" :"http://www.techbeamers.com/selenium-webdriver-python-tutorial/",
+    "abstract" :"Selenium WebDriver is one of the most popular tools for Web UI Automation. And no better than Python can complement it to automate a broad range of web ...",
+    "title" :"Selenium Webdriver Python Tutorial for Web Automation Testing",
+    },
+    {
+    "url" :"http://stackoverflow.com/questions/9945206/can-web-automation-be-done-in-python",
+    "abstract" :"2012年3月30日 - I am a newborn programmer still programming from the book on my ... I think you should take a look at the Mechanize project. It is a stateful ...",
+    "title" :"Can web automation be done in Python? - Stack Overflow",
+    },
+    {
+    "url" :"http://stackoverflow.com/questions/1292817/how-to-automate-browsing-using-python",
+    "abstract" :"2009年8月18日 - You can also take a look at mechanize. Its meant to handle stateful programmatic web browsing (as per their site).",
+    "title" :"browser automation - How to automate browsing using python ...",
+    },
+    {
+    "url" :"https://medium.com/the-python-corner/web-test-automation-in-python-a319a0783187",
+    "abstract" :"2016年11月15日 - Web-Test Automation in Python. A couple of days ago I was in the need of automating a simple browsing on internet. To be more specific I ...",
+    "title" :"Web-Test Automation in Python – The Python corner – Medium",
+    },
+    {
+    "url" :"https://splinter.readthedocs.io/",
+    "abstract" :"Splinter is an open source tool for testing web applications using Python. It lets you automate browser actions, such as visiting URLs and interacting with their ...",
+    "title" :"Splinter — Splinter 0.7.5 documentation",
+    },
+    {
+    "url" :"http://splinter.readthedocs.io/en/0.1/",
+    "abstract" :"Documentation for splinter, an open source tool for testing web applications.",
+    "title" :"automate web application actions using python — splinter v0.1 ...",
+    },
+    {
+    "url" :"http://www.seleniumhq.org/",
+    "abstract" :"Selenium automates browsers. That's it! What you do with that power is entirely up to you. Primarily, it is for automating web applications for testing purposes, but ...",
+    "title" :"Selenium - Web Browser Automation",
+    },
+    {
+    "url" :"https://www.reddit.com/r/Python/comments/1nk0r4/python_library_for_automating_interaction_with_a/",
+    "abstract" :"2013年10月2日 - Hi all, a python newb here, There's a web-tool I would like to use. When I interact with the webpage manually. The steps of interaction are: 1)...",
+    "title" :"Python library for automating interaction with a webpage? : Python",
+    },
+    {
+    "url" :"https://answers.squarespace.com/questions/120459/web-automation-in-python.html",
+    "abstract" :"2016年1月25日 - I'm finishing off learning python and want to create a webpage where my users can log into site b, via my site, (site a). So basically is there ...",
+    "title" :"Web automation in python? - Answers",
+    },
+    ],
 }
 keyword_list = [ 
-   "python  web automation",
-   "how to automate browsing using python",
+   "   关系",
+   "   可视化",
+   "   4因5路",
+   "   排列组合",
+   "   边际效应",
+   "   费米计算",
+   "   增长",
+   "   从心开始",
+   "   证伪",
+   "   租",
+   "   交换",
+   "   外延",
+   "   统一",
+   "   生老病死",
+   "   推动",
+   "   有用",
+   "   极限",
+   "   未来",
+   "   混沌",
+   "   高维",
+   "   不动点",
+   "   分解",
+   "   建模",
+   "   局部化",
+   "   为什么",
 ]
-meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+meta_data = {"status":".","Major": 1,"Minor": 2,"Prefix": "Q"}
 km_0_title = "automate browing using python"
 km_0_summary = "ummary_tp"
 km_1_tags = []
