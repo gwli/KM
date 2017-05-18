9cf6c9043b8703234a0e11c600438c0d48ab7d0e
victor <gwli@outlook.com>
2017-05-18 09:17:55 +0800

    automate web browsing

---
+++ source/Python_web_automation.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "python  web automation",
+   "how to automate browsing using python",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "automate browing using python"
+km_0_summary = "ummary_tp"
+km_1_tags = []
