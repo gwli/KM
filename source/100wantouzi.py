209eec1be7af558a00f1931dc27839898d70fa4d
victor <gwli@outlook.com>
2017-05-19 09:25:41 +0800

    添加一个新课题

---
+++ source/100wantouzi.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "100万 如何投资",
+   "100万 能干什么",
+   "如何从100万 到10000亿",
+   "未来有哪些可投资 项目",
+   "100的万复利 ",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "100万如何投资"
+km_0_summary = "ummary_tp"
+km_1_tags = []
