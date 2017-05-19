686c0eb9c1f3a3708eaa4e2d1ee8a8919f394991
victor li <vili@nvidia.com>
2017-05-20 00:01:01 +0800

    nb 2017-05-20

--- source/100wantouzi.py
+++ source/100wantouzi.py
@@ -2,17 +2,317 @@
 
 Q_dict = { 
 ######
-#Q_0_0 #:adfadfafa: : : : 
+#Q_1_4 #:100的万复利 
 ######
+"Q_1_4": ["100的万复利 ",
+    {
+    "url" :"https://www.cmoney.tw/notes/note-detail.aspx?nid=6569",
+    "abstract" :"善用複利，幫你錢滾錢。 投資報酬率的公式很簡單： 淨利 / 投入本金 講白話一點就是： 賺的錢 / 投入的錢. 舉個例子：. 假設我花100萬買台積電股票 一年後漲到120萬 ...",
+    "title" :"一分鐘搞懂複利的觀念 - CMoney",
+    },
+    {
+    "url" :"http://www.masterhsiao.com.tw/MoneyTimeValue/CompoundInterest/CompoundInterest.html",
+    "abstract" :"複利一定有付息期間，也就是在當期的最後一天，才將利息加到本金上，在期間內所 ... 例一：張三跟朋友借款100萬元，約定利息以年利率6%『單利』計算，3年後還款。",
+    "title" :"實質年利率公式 - 複利-完全攻略篇",
+    },
+    {
+    "url" :"https://zh.wikipedia.org/zh-hk/%E5%A4%8D%E5%88%A9",
+    "abstract" :"複利率法（英文：compound interest），是一种计算利息的方法。按照这种方法，利息除了會根據本金 ... 只有100萬元，整體財富增長只是10倍，但按照複利方法計算，首年回報是10萬元，令個人整體財富變成20萬，第二年20萬會變成40萬，第三年40萬 ...",
+    "title" :"复利- 维基百科，自由的百科全书",
+    },
+    {
+    "url" :"http://supershingetter.pixnet.net/blog/post/38953522-%E8%A4%87%E5%88%A9%E9%81%94%E4%BA%BA%E3%80%8C%E6%80%AA%E8%80%81%E5%AD%90%E3%80%8D%E8%95%AD%E4%B8%96%E6%96%8C%EF%BC%8C%E6%AF%8F%E5%B9%B4%E5%9D%90%E4%BA%AB%E7%99%BE%E8%90%AC%E9%80%B2",
+    "abstract" :"2011年11月15日 - 只不過搞懂一個觀念&mdash;&mdash;什麼是「複利」？ ... 現金、外加新北市一間貸款已經還清的房子，而且不必工作，每年就能至少進帳100萬元。",
+    "title" :"複利達人「怪老子」蕭世斌，每年坐享百萬進帳10年存款翻5倍 - 千千靜思語",
+    },
+    {
+    "url" :"http://baike.baidu.com/item/%E5%A4%8D%E5%88%A9%E8%AE%A1%E6%81%AF/8992066",
+    "abstract" :"举例来说，假定某投资每年有10%的获利，若以单利计算，投资100万元，每年可赚10万元，十年可以赚100万元，多出一倍。但如果以复利计算，虽然年获利率 ...",
+    "title" :"复利计息_百度百科",
+    },
+    {
+    "url" :"http://investarter.blogspot.com/2012/10/blog-post_16.html",
+    "abstract" :"你現在有五種投資工具 它們的年報酬率分別是1%、5%、10%、15%、20%. 一開始本金都從100萬開始 每年都經過複利計算經過30年 我們來看看這100萬會變成多少 ...",
+    "title" :"阿升理財筆記: 什麼是投資報酬率? 什麼是複利?",
+    },
+    {
+    "url" :"http://selfinsurance1911financenote.blogspot.com/2015/04/1-07725.html",
+    "abstract" :"2015年4月21日 - 只要用一個簡單的公式：「72/年化報酬率= 本金翻倍的年數」。比如，年化報酬率2%的情況，可以這樣速算：72/2=36。代表大約36年的時間，100萬就 ...",
+    "title" :"神奇的複利72法則 - 效率理財王 - blogger",
+    },
+    {
+    "url" :"http://richjamie.blogspot.com/2014/06/blog-post_9.html",
+    "abstract" :"2014年6月9日 - 所以在進入投資之前必須先搞懂單利跟複利的差別. 假設本金100萬. 投資在【單利】10%的工具，10年下來的結果是200萬. 投資在【複利】10%的 ...",
+    "title" :"輕易豐盛學苑: 誇張~你知道複利10%竟然可以打敗單利50%嗎？",
+    },
+    {
+    "url" :"https://terence0425.wordpress.com/2014/11/27/%E5%A6%82%E4%BD%95%E5%B0%87300%E5%85%83%E8%AE%8A%E6%88%90100%E8%90%AC%EF%BC%9F/",
+    "abstract" :"2014年11月27日 - 三項煉金術如何將300元變成100萬元呢？這個看似“白日做夢”的 ... 複利嚇死人複利，也就是把所賺到的錢再進行投資，讓錢再生錢。如果能讓複利的 ...",
+    "title" :"如何將300元變成100萬？ | 朋友．珍惜你所擁有的",
+    },
+    {
+    "url" :"http://leoson333.pixnet.net/blog/post/29760378-%E6%8A%95%E8%B3%87%E7%90%86%E8%B2%A1---%E8%A4%87%E5%88%A9%E7%9A%84%E9%AD%94%E5%8A%9B",
+    "abstract" :"在投資理財的話題中，我們常聽到「複利」是理財獲利的關鍵，到底它有什麼魔力，今天 ... 舉例來說，假定某項投資每年有一0％的獲利，若以單利計算，投資一百萬元， ...",
+    "title" :"投資理財- 複利的魔力@ 悠遊股海:: 痞客邦PIXNET ::",
+    },
+    ],
+######
+#Q_1_1 #:100万 能干什么
+######
+"Q_1_1": ["100万 能干什么",
+    {
+    "url" :"https://www.douban.com/group/topic/36260713/",
+    "abstract" :"2013年1月25日 - 老妈给我两个选择，留学or自己创业 一百万的资金，却不知道做什么好，怕失败。 兴趣在于服装和烘焙，毕竟也学过5年的画画，妹妹在烘焙方面也 ...",
+    "title" :"一百万能干什么 - 豆瓣",
+    },
+    {
+    "url" :"https://www.zhihu.com/question/22590902",
+    "abstract" :"题主的问题是如何白手起家靠一个人赚到100万，我觉得这个是可以做到的，但是 ..... 分析得了当前市场，却永远不可能分析出'隐形机会' 对了，那聪明人适合干什么？",
+    "title" :"如何白手起家挣到一百万？ - 知乎",
+    },
+    {
+    "url" :"http://penny926.pixnet.net/blog/post/26702924-100%E8%90%AC%E5%8F%AF%E4%BB%A5%E5%81%9A%E4%BB%80%E9%BA%BC%3F",
+    "abstract" :"我一直以為，100萬可以讓小孩大一唸到大四！所以，只要我存錢，我的小孩到大學畢業都不用貸款！基本上，現在的100萬在台灣唸完大學應該沒問題！省一點應該夠 ...",
+    "title" :"100萬可以做什麼? @ Penny926的心情日記:: 痞客邦PIXNET ::",
+    },
+    {
+    "url" :"https://kknews.cc/zh-hk/news/bxl95k9.html",
+    "abstract" :"2016年12月8日 - 講真，現在，錢越來越不值錢了！兜里揣上100塊都不敢粗去耍話說，小時候手拿5毛錢都覺得自己財大氣粗！！來，唐僧肉給我來一包！不，兩包！",
+    "title" :"誰看誰震驚！十年後100萬在太原值多少錢？能幹什麼？ - 每日頭條",
+    },
+    {
+    "url" :"http://app.myzaker.com/news/article.php?pk=54f42eed1bc8e0c77f000032",
+    "abstract" :"你想知道现在的100 万在10 年、20 年后的上海能值多少钱吗？那时的100 万在上海可以做什么？你还可以任性起来么？小魔带您进入这个未来世界，拿着你手上的 ...",
+    "title" :"20 年后100 万人民币在上海能干什么 - ZAKER",
+    },
+    {
+    "url" :"http://www.sohu.com/a/133543124_382805",
+    "abstract" :"2017年4月12日 - 100万有多少？100万能干什么？ 60年代. 60年代末，10元能买12斤大闸蟹！ 70年代. 这简直就是土豪的代名词。那时候米价0.14元，肉价0.95元，走 ...",
+    "title" :"给你一百万，你想干什么？美女竟然这样回答！ - 搜狐",
+    },
+    {
+    "url" :"http://www.p2peye.com/thread-487537-1-1.html?mobile=2",
+    "abstract" :"2015年8月6日 - 按照购买能力计算，现在的100万元，10年后的真实购买力≈现在的32-62万元 ... So，现在的100万在10年、20年后的上海，能干什么？ ... ,网贷天眼.",
+    "title" :"100万20年后能做什么？-投资交流-网贷天眼",
+    },
+    {
+    "url" :"https://www.ptt.cc/bbs/CFP/M.1253551850.A.1CE.html",
+    "abstract" :"2009年9月22日 - 目前正在朝著中期目標50萬元邁進！！ : 但我常在想，到底存到100萬能做什麼你應該是先想你的目標是啥需要哪些材料來達成如果100萬資金是 ...",
+    "title" :"Re: [問題] 存到100萬到底可以做什麼？ - 看板CFP - 批踢踢實業坊",
+    },
+    {
+    "url" :"http://article.yeeyan.org/view/263766/243816",
+    "abstract" :"2012年1月3日 - 面对这个著名的100万美金问题，很多人只会一笑置之——既然没有，何必操心？本文作者却认真思考，提出切实的想法。她还问道：要是有10亿呢？",
+    "title" :"译言-如果我有100万美金，会用来做什么？ - 译言网",
+    },
+    {
+    "url" :"http://zhidao.baidu.com/question/1831143057946245820.html",
+    "abstract" :"2014年12月3日 - 6000左右看你怎么用.",
+    "title" :"100万韩币在韩国能干什么，等于多少人民币_百度知道",
+    },
+    ],
+######
+#Q_1_0 #:100万 如何投资
+######
+"Q_1_0": ["100万 如何投资",
+    {
+    "url" :"http://www.mpfinance.com/fin/columnist2.php?col=1463481152597&node=1486062188880&issue=20170203",
+    "abstract" :"2017年2月3日 - 試想像，即使每月儲50%收入，加上通脹等因素，也要儲足15年才有100萬元；但如果只存20萬元本金，再利用理財投資規劃，把本金滾大，只需約 ...",
+    "title" :"張佩儀﹕善用價值投資4年半20萬變100萬- 20170203 - 專家觀點張佩儀 ...",
+    },
+    {
+    "url" :"https://www.zhihu.com/question/20162904",
+    "abstract" :"2015年5月20日 - 因为中国经济增长率不可能永远维持高水平，投资收益率也不可能永远保持高位，要接受投资收益率逐渐平稳下降的事实。 在用100万元进行投资 ...",
+    "title" :"手里有100万，如何理财？ - 知乎",
+    },
+    {
+    "url" :"https://www.zhihu.com/question/38574039",
+    "abstract" :"2017年2月17日 - 手里有父母给的买房钱100万，不想买房，一是现在房地产价格上涨空间不大，不着急买，二是想利用这些钱生钱，六月份股市最火爆的时候拿了20万 ...",
+    "title" :"手里有100万如何投资？ - 知乎",
+    },
+    {
+    "url" :"http://www2.hkej.com/instantnews/market/article/1388151",
+    "abstract" :"2016年9月11日 - 讀者杜小姐問：「打算年底前賣樓，然後用100萬投資股票，一年回報20%，以補貼全年租金開支。請問各位專家有何選股建議，持多少隻股票才合適？",
+    "title" :"【一問三答】賣樓後用100萬買股，想年賺20%.... - 即時新聞 - 信報網站",
+    },
+    {
+    "url" :"http://www.edigest.hk/%E5%B0%88%E6%AC%84/%E6%9C%89100%E8%90%AC%E4%BF%82%E6%89%8B%EF%BC%8C%E5%8F%AF%E4%BB%A5%E9%BB%9E%E6%8A%95%E8%B3%87%EF%BC%9F/",
+    "abstract" :"2016年9月8日 - Homeblogger人氣博客諗Sir分享樓市熱話及投資心得！ 以下是諗Sir發言重點：. 以下提及的投資組合・REITS ・中國人壽(02628) ・領展(00823)",
+    "title" :"有100萬係手，可以點投資？ | 專欄| 經濟一週",
+    },
+    {
+    "url" :"http://wealth.businessweekly.com.tw/GArticle.aspx?id=ARTL000079354",
+    "abstract" :"2017年1月9日 - 二、資金20萬～100萬：先存下第一桶金. 別懷疑，即使是20萬～100萬資金仍是太少，當手邊資金少時，每個人都很想靠投資賺大錢、一次翻身。",
+    "title" :"「我有20萬，可以投資什麼？」一次解答20萬、100萬、500萬 ... - 商周財富網",
+    },
+    {
+    "url" :"http://lifestyle.etnet.com.hk/column/index.php/wealth/wealthmanagement/32233",
+    "abstract" :"2015年5月15日 - 用100萬賺1000萬，似乎是不可能的，可以靠甚麼方法呢？ 投資有很多方法，如果不靠股票投資的話是絕對不可能做得到。當然，你可以投資房地產， ...",
+    "title" :"從100萬到1,000萬，十年賺十倍（上） - 方元第一桶金- 財智- 生活副刊 ...",
+    },
+    {
+    "url" :"http://www.cheers.com.tw/article/article.action?id=5036621",
+    "abstract" :"小職員也能有百萬夢？ 答案絕對是肯定的！ 在只有「物價漲、薪水不漲」的時代裡， 「駕馭金錢」，是每個上班族都要有的技能。 如何用薄薄的薪水，滾出厚厚的百萬 ...",
+    "title" :"靠月薪，2年滾出100萬！ - 懂投資- 理財- Cheers快樂工作人雜誌",
+    },
+    {
+    "url" :"https://kknews.cc/zh-hk/finance/4845vqv.html",
+    "abstract" :"2017年1月10日 - 投資理財已經成為一種時代要求。接下來，看看錢耳朵（ID：wealthear）為客戶配置的100萬資金的理財方案，我相信或多或少會給到投資者一些幫助。",
+    "title" :"手裡有100萬，如何投資理財？ - 每日頭條",
+    },
+    {
+    "url" :"http://cht.86722.com/licai/i102150/",
+    "abstract" :"家有100萬如何投資理財好？ 科學理財，優化分配。存款儲蓄作為一種投資行為，與債券、股票、保險相比風險低，但收益小，無流通性。可那出20萬來投資儲蓄。債券分 ...",
+    "title" :"家有100萬如何投資理財好？ - 如何理財投資- 86722理材網",
+    },
+    ],
+######
+#Q_1_3 #:未来有哪些可投资 项目
+######
+"Q_1_3": ["未来有哪些可投资 项目",
+    {
+    "url" :"http://big5.58cyjm.com/wenda/cyys/21736.shtml",
+    "abstract" :"補充： 我想創業，請問現在比較賺錢的創業項目都有哪些？ ... 下面為你分析在未來創業比較賺錢的項目，還是老話：投資有風險，創業需謹慎。 ... 區商圈; · 如何考察創業項目: 創業初期，對於目標項目的考察，可以說是較為關鍵也是較讓人費神並且的。",
+    "title" :"未來賺錢的創業項目有哪些- 創業要素 - 58創業加盟網",
+    },
+    {
+    "url" :"https://kknews.cc/zh-hk/agriculture/qea646y.html",
+    "abstract" :"2017年1月12日 - 最新農村創業項目，該項目投資較少、風險性小、市場前景廣闊。 ... 農村網際網路的發展，一方面可以把很多產品帶到農民家庭里去，一方面也應該 ...",
+    "title" :"未來農村有哪些項目最賺錢?40個農村創業致富項目- 每日頭條",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=_aYnrpCuF7kC&pg=PT210&lpg=PT210&dq=%E6%9C%AA%E6%9D%A5%E6%9C%89%E5%93%AA%E4%BA%9B%E5%8F%AF%E6%8A%95%E8%B5%84+%E9%A1%B9%E7%9B%AE&source=bl&ots=MQtzD1sMTw&sig=MITtrIwR4jAbKJUPdu3tUmA6JEg&hl=zh-TW&sa=X&ved=0ahUKEwju_tiTqPzTAhXJbbwKHbN9CVEQ6AEILTAC",
+    "abstract" :"生产性资产投资的主要类型生产性资本资产投资项目可以分为 5 类: ( 1 )新产品 ... 实际) ,改善财务控制的线索(执行中有哪些地方出了问题) ,有助于指导未来决策(哪 一 ...",
+    "title" :"经济学基础 - Google 圖書結果",
+    "metadata" :"2004 - Economics",
+    },
+    {
+    "url" :"https://kknews.cc/zh-hk/agriculture/g2e2r8e.html",
+    "abstract" :"未來要靠這幾樣東西賺錢，適合農村創業的項目. 2017-01-26 由 莫 .... 有條件者，可投資一萬元左右，購一些農民必用物品出租，收取租金。可出租的商品 ... 那麼在農村如何賺錢?現在適合農村的農業項目很多，那麼都有哪些農業致富項目可以賺錢呢?",
+    "title" :"未來要靠這幾樣東西賺錢，適合農村創業的項目- 每日頭條",
+    },
+    {
+    "url" :"https://read01.com/Q4o2k2.html",
+    "abstract" :"2017年2月27日 - 是否還記得馬雲說未來賺錢的行業都有哪些，他認為未來最有前景的十大行業究竟是什麼， ... 嬰兒游泳館你可以關注一下，不過這個項目投資不少。",
+    "title" :"2017年馬雲說未來賺錢的行業都有哪些？網際網路小本創業怎麼選 ...",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=8ytUDQAAQBAJ&pg=PT351&lpg=PT351&dq=%E6%9C%AA%E6%9D%A5%E6%9C%89%E5%93%AA%E4%BA%9B%E5%8F%AF%E6%8A%95%E8%B5%84+%E9%A1%B9%E7%9B%AE&source=bl&ots=D9NGOJSeEG&sig=nXQepMXga1lNP_M8PspB1TeoWLo&hl=zh-TW&sa=X&ved=0ahUKEwju_tiTqPzTAhXJbbwKHbN9CVEQ6AEINzAF",
+    "abstract" :"这些投资项目的现金流量分布有不同的特征,分析的具体方法也有区别。 ... 项目的事后评价可以告诉我们预测的偏差(我们的预测在什么地方脱离了实际),改善财务 ... (执行中有哪些地方出了问题),有助于指导未来决策(哪类项目值得实施或不值得实 施)。",
+    "title" :"财务成本管理 - Google 圖書結果",
+    "metadata" :"中国注册会计师协会编, 宋学军 - 2012 - Business & Economics",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=VUnjePG9IRIC&pg=PA382&lpg=PA382&dq=%E6%9C%AA%E6%9D%A5%E6%9C%89%E5%93%AA%E4%BA%9B%E5%8F%AF%E6%8A%95%E8%B5%84+%E9%A1%B9%E7%9B%AE&source=bl&ots=JQuqap8Pj6&sig=5Zg-ulTBda8iuyD-377ZXSuucwE&hl=zh-TW&sa=X&ved=0ahUKEwju_tiTqPzTAhXJbbwKHbN9CVEQ6AEIPDAG",
+    "abstract" :"4 ·项目的未来市场预测评估项目的未来市场预测评估主要分析: 0 产品的潜在消费群 ... 风险投资分析的方法风险投资分析常用的专门方法有很多,但通常可以分为两类:一类 ... 阻碍中国风险投资业发展的主要因素有哪些,如何推动中国风险投资业的发展, ...",
+    "title" :"投资学 - 第 382 頁 - Google 圖書結果",
+    "metadata" :"2004 - Corporations",
+    },
+    {
+    "url" :"http://www.wuaiwenxue.com/chuangye/zhuanqian/35.html",
+    "abstract" :"2016年7月12日 - 什么行业最具有前景、最能赚钱一直是投资者和创业者关注的问题，那2017年哪些行业最有前途 ... 美文网带你一起分析未来最有赚钱潜力的十大行业 ... 2017年创业项目推荐2、教育和培训行业. 中国适龄劳动人口基数巨大，劳动力技术技能培养的需求也是巨大的，这个行业的潜力从新东方火热上市就可以看出端倪。",
+    "title" :"2017年做什么生意最赚钱2017最有赚钱潜力的十大行业-吾爱文学网",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=CgvNDgAAQBAJ&pg=PA141&lpg=PA141&dq=%E6%9C%AA%E6%9D%A5%E6%9C%89%E5%93%AA%E4%BA%9B%E5%8F%AF%E6%8A%95%E8%B5%84+%E9%A1%B9%E7%9B%AE&source=bl&ots=6_DKgHzKgf&sig=EarLoyC899LQ9-SDHsJEEw1cPgI&hl=zh-TW&sa=X&ved=0ahUKEwju_tiTqPzTAhXJbbwKHbN9CVEQ6AEISTAI",
+    "abstract" :"对一些拥有大型投资组合的投资者,可以在投资组合中标的资产(如股票)的支持下进行期权 ... 在传统的财务投资决策中,净现值)法被广泛应用于评价投资项目的可行性,即对于某一投资项目,如果未来现金 ... 期权作为一种金融商品具有哪些显著特点?",
+    "title" :"财务学原理:  - 第 141 頁 - Google 圖書結果",
+    "metadata" :"主 　 编 ： 李 　 君 副 主 编 ： 苏 　 浩 　 余 鹏",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=l0cMDgAAQBAJ&pg=PA55&lpg=PA55&dq=%E6%9C%AA%E6%9D%A5%E6%9C%89%E5%93%AA%E4%BA%9B%E5%8F%AF%E6%8A%95%E8%B5%84+%E9%A1%B9%E7%9B%AE&source=bl&ots=meYCDTTyqR&sig=v789WYpq-UmhUVgY2dk8KAZJQAU&hl=zh-TW&sa=X&ved=0ahUKEwju_tiTqPzTAhXJbbwKHbN9CVEQ6AEITjAJ",
+    "abstract" :"但一旦未來投資者賣出港股,資金便會原路返回,所以它是一個封閉的、收益可交換的機制。 ... 正是因為這種可控、可追溯的資本項目開放機制,即使目前短期人民幣面臨資金流出壓力,決策者還是取消了交易的總額度限制。互聯互通的交易主要有哪些特點呢? ... 可供未來擴容,具有可擴充性,下一步可以延伸到一級市場、新股的發行市場和 ...",
+    "title" :"從滬港通到深港通—互聯互通與香港新機遇:  - 第 55 頁 - Google 圖書結果",
+    "metadata" :"彭琳, 梁海明 - 2017",
+    },
+    ],
+######
+#Q_1_2 #:如何从100万 到10000亿
+######
+"Q_1_2": ["如何从100万 到10000亿",
+    {
+    "url" :"https://zhuanlan.zhihu.com/p/21784812",
+    "abstract" :"2016年7月29日 - CEO寄语：. 你躺在回龙观的合租房里，感觉身体被掏空，写字台上吃完的黄焖鸡米饭还没来得及收拾，好久不联系的女神忽然发来信息：明天晚上 ...",
+    "title" :"【JOIN产品实战笔记】市值100万到10000亿的App，都是如何设计筛选 ...",
+    },
+    {
+    "url" :"http://www.woshipm.com/pd/382988.html",
+    "abstract" :"2016年8月1日 - 百度前产品教父俞军曾言: “用户体验，就是让用户付出最小成本满足需求”。 你躺在回龙观的合租房里，感觉身体被掏空，写字台上吃完的黄焖鸡米饭 ...",
+    "title" :"市值100万到10000亿的App，都是如何设计筛选功能的？ | 人人都是 ...",
+    },
+    {
+    "url" :"http://www.sohu.com/a/131712526_476025",
+    "abstract" :"复盘葛卫东投资平安银行的故事，以及葛卫东从100万到120亿的传奇经历、葛卫东 ... 完成收购之后，葛卫东创造了交易员到私募大鳄再到控股期货公司的蜕变。 .... 比如6000点大家看着能涨到8000点、10000点，但是它就不涨，它开始掉头了，这就是 ...",
+    "title" :"葛卫东：100万如何做到120亿-财经频道-手机搜狐",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=3BT9CAAAQBAJ&pg=PA225&lpg=PA225&dq=%E5%A6%82%E4%BD%95%E4%BB%8E100%E4%B8%87+%E5%88%B010000%E4%BA%BF&source=bl&ots=jxLLZz26pp&sig=Vdyeco5JsPL9XJwVv9kLTBk_1b4&hl=zh-TW&sa=X&ved=0ahUKEwiVtOyPqPzTAhUBypQKHaGzBnsQ6AEIOzAD",
+    "abstract" :"所以每月利息淨支出= 22,000 - 12,000 = 10,000 兩年累計支出= 10,000×24 ... 甚至自備五十萬也可以買屋,買個五百萬的房子投資,同樣兩年後也會賺到 50 到 100 萬 ...",
+    "title" :"從零開始賺一億: 呂原富教你看準時機，錢進房市，資產大翻身！",
+    "metadata" :"呂原富 - 2015 - Business & Economics",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=qbxTDQAAQBAJ&pg=PT342&lpg=PT342&dq=%E5%A6%82%E4%BD%95%E4%BB%8E100%E4%B8%87+%E5%88%B010000%E4%BA%BF&source=bl&ots=3nblySbpJE&sig=3IfbpYbLyCHeIPwm0Iv6BHJopvI&hl=zh-TW&sa=X&ved=0ahUKEwiVtOyPqPzTAhUBypQKHaGzBnsQ6AEIQDAE",
+    "abstract" :"因此,2009年3月10日又公布了第二个经济刺激方案,总额达600亿林吉特(约合160亿美元),是马来西亚有史以来动用最大 ... 从2008年7月1日起,失业补偿费免征所得税的限额从原来的6000 林吉特提高到10000林吉特。 ... 减税措施,从2009年1月1日起实施,预计减税100亿泰铢。 ... 现在,最低选择税的起征点从6万泰铢提高到100万泰铢。",
+    "title" :"中国税收研究报告 - Google 圖書結果",
+    "metadata" :"国家税务总局税收科学研究所编, 刘五书 - 2010 - Business & Economics",
+    },
+    {
+    "url" :"https://read01.com/M8kQkm.html",
+    "abstract" :"2016年8月29日 - 你躺在回龍觀的合租房裡，感覺身體被掏空，寫字檯上吃完的黃燜雞米飯還沒來得及收拾，好久不聯繫的女神忽然發來信息：明天晚上國貿附近一起吃 ...",
+    "title" :"市值100萬到10000億的App，都是如何設計篩選功能的？ - 壹讀",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=uEj3AwAAQBAJ&pg=PT48&lpg=PT48&dq=%E5%A6%82%E4%BD%95%E4%BB%8E100%E4%B8%87+%E5%88%B010000%E4%BA%BF&source=bl&ots=ZDHQbhoV48&sig=aLvxE6YYHyFOv8frUFwfv2Y-P6c&hl=zh-TW&sa=X&ved=0ahUKEwiVtOyPqPzTAhUBypQKHaGzBnsQ6AEISDAG",
+    "abstract" :"收好那張 10000 元的後,他把那張 20000 元遞給了前臺收銀的。現在,他心裏又踏實了一些。畢竟這 100 萬是真金白銀。花去 2 萬,也還有 98 萬。在拿到 900 億以前, ...",
+    "title" :"未知星球: ... - Google 圖書結果",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=6qQnDQAAQBAJ&pg=PA98&lpg=PA98&dq=%E5%A6%82%E4%BD%95%E4%BB%8E100%E4%B8%87+%E5%88%B010000%E4%BA%BF&source=bl&ots=nmUY0vLZRs&sig=4MgEx3Z2g1yxwjSj0xnhAjDrckQ&hl=zh-TW&sa=X&ved=0ahUKEwiVtOyPqPzTAhUBypQKHaGzBnsQ6AEISjAH",
+    "abstract" :"甲棟:100 萬(頭期款)+ 400 萬(貸款),也是二十年分期,前三年只繳利息。 ... 貸款 400 萬,每棟需繳交的利息每月是 10,000 元,一棟三年要繳 36 萬,兩棟加起來共需繳 72 ...",
+    "title" :"從0開始賺1億(2)：是真的！房產天王呂原富教你零元購屋: ",
+    "metadata" :"呂原富 - 2016 - Business & Economics",
+    },
+    {
+    "url" :"https://zh.wikipedia.org/zh-hk/%E4%B8%96%E7%95%8C%E4%BA%BA%E5%8F%A3",
+    "abstract" :"非洲是世界第二多人口的大洲，約有十億人，佔全世界人口15%；歐洲有7.33億人， ... 從那時起，世界總人口長期停滯在一百萬左右，大家都通過狩獵採集為生，而這種 ...",
+    "title" :"世界人口- 維基百科，自由的百科全書 - Wikipedia",
+    },
+    {
+    "url" :"https://kknews.cc/zh-hk/house/jvo3zop.html",
+    "abstract" :"市場上流傳這樣一個段子：京滬深三地房屋現在總市值約100萬億，這意味著，賣掉京滬深 ... 滬指5178點時，市場也曾充斥著樂觀情緒，看多到10000點的人大有人在。",
+    "title" :"京滬深房產值100萬億，能買半個美國！樓市5178還遠嗎？ - 每日頭條",
+    },
+    ],
 }
 keyword_list = [ 
-   "100万 如何投资",
-   "100万 能干什么",
-   "如何从100万 到10000亿",
-   "未来有哪些可投资 项目",
-   "100的万复利 ",
+   "   关系",
+   "   联想",
+   "   4因5路",
+   "   计算复杂度:常，对，线，平，立，指，阶",
+   "   复利",
+   "   费米计算",
+   "   迭代",
+   "   从心开始",
+   "   验证",
+   "   租",
+   "   优化",
+   "   外延",
+   "   统一",
+   "   健康",
+   "   展示",
+   "   有用",
+   "   奇点",
+   "   未来",
+   "   混沌",
+   "   高维",
+   "   不动点",
+   "   分块",
+   "   建模",
+   "   局部化",
+   "   时间",
 ]
-meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+meta_data = {"status":".","Major": 1,"Minor": 5,"Prefix": "Q"}
 km_0_title = "100万如何投资"
 km_0_summary = "ummary_tp"
 km_1_tags = []
