8b27a0b4f44ed44f0e289142329b4fda92983291
victor li <vili@nvidia.com>
2017-05-20 15:45:33 +0800

    add quanli

---
+++ source/quanli.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+}
+keyword_list = [ 
+   "孔子孔子“仁”观念的渊源与本质",
+   " 编户齐民与宗族聚居共存的原因。",
+   " 封建制、宗法关系、编户齐民的关系与演变",
+   " 权 力 与昨权 力 寻租 的数 学模 型 ",
+]
+meta_data = {"status":"mc","Major": 1,"Minor": 20,"Prefix": "Q"}
+km_0_title = "权力"
+km_0_summary = "ummary_tp"
+km_1_tags = []
