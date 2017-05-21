40c135dbe7a8433f11b1bebd87c8c04d7f5bc149
victor <gwli@outlook.com>
2017-05-21 20:28:52 +0800

    添加了信任

---
+++ source/XinRen/XinRen.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "信任 与义气",
+   "信任 与礼仪",
+   "如何快速建立信任",
+   "信任的本质及其文化",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "信任"
+km_0_summary = "ummary_tp"
+km_1_tags = ["期望","喜欢","礼仪"]
