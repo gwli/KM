7b33f66726a3f6075b30aeedc9d58486c38ac625
victor li <vili@nvidia.com>
2017-05-22 00:00:57 +0800

    nb 2017-05-22

--- source/XinRen/XinRen.py
+++ source/XinRen/XinRen.py
@@ -2,16 +2,254 @@
 
 Q_dict = { 
 ######
-#Q_0_0 #:adfadfafa: : : : 
+#Q_1_1 #:信任 与礼仪
 ######
+"Q_1_1": ["信任 与礼仪",
+    {
+    "url" :"https://books.google.com.hk/books?id=BhVxBAAAQBAJ&pg=PT99&lpg=PT99&dq=%E4%BF%A1%E4%BB%BB+%E4%B8%8E%E7%A4%BC%E4%BB%AA&source=bl&ots=se7H_awMw4&sig=g9F_WkHb62k5W_oJqBtU7t7t0vA&hl=zh-TW&sa=X&ved=0ahUKEwjV8ZPmq4HUAhVGKZQKHXvDDHUQ6AEINzAF",
+    "abstract" :"守诺守时,会让人对你充满信任*要别人信任,首先得自己值得信一个人具备一定的能力素质,同时说明了其也具有一定的内在潜力,要想把这种内在潜力挖掘出来,信任是 ...",
+    "title" :"礼仪是一种资本·日常礼仪的300个细节 - Google 圖書結果",
+    "metadata" :"李洁, Esphere Media（美国艾思传媒） - 2007",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=TjZzBQAAQBAJ&pg=PT34&lpg=PT34&dq=%E4%BF%A1%E4%BB%BB+%E4%B8%8E%E7%A4%BC%E4%BB%AA&source=bl&ots=0gzrG-OSAB&sig=I7TEPG5zcpBc4IEJkQcnVhr-vZI&hl=zh-TW&sa=X&ved=0ahUKEwjV8ZPmq4HUAhVGKZQKHXvDDHUQ6AEIPDAG",
+    "abstract" :"第5章精当措辞,提高说服力增强与领导的沟通力与领导谈话,礼貌用语要用好 我们生活的国家是著名的礼仪之邦,讲究礼数向来是中国人引以为傲的传统美德。语言是 ...",
+    "title" :"如何沟通领导才放心怎么做事领导才信任大全集:  - Google 圖書結果",
+    "metadata" :"赵凡禹 - 2014",
+    },
+    {
+    "url" :"http://www.lifer.tw/blog/post/109487288-%E5%A6%82%E4%BD%95%E9%81%B8%E6%93%87%E4%BB%A4%E4%BA%BA%E6%94%BE%E5%BF%83%E7%9A%84%E6%AE%AF%E8%91%AC%E6%A5%AD%E8%80%85%3F",
+    "abstract" :"Jun 07 Tue 2016 17:42. 密碼文章 如何選擇令人安心信任放心的禮儀公司:大剖析怎麼挑選殯葬業者家屬心聲:睜大眼才能找到評價好的禮儀公司風評佳的葬儀社不踩 ...",
+    "title" :"如何選擇令人安心信任放心的禮儀公司:大剖析怎麼挑選殯葬業者家屬 ...",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=luSWCwAAQBAJ&pg=PT117&lpg=PT117&dq=%E4%BF%A1%E4%BB%BB+%E4%B8%8E%E7%A4%BC%E4%BB%AA&source=bl&ots=iKnDm6xxIt&sig=aaq9MxpXSHHSzjgcy_foDyaaFo4&hl=zh-TW&sa=X&ved=0ahUKEwjV8ZPmq4HUAhVGKZQKHXvDDHUQ6AEIRjAI",
+    "abstract" :"评价一个人是否懂礼仪、有修养,不仅要看他的穿着打扮、言谈举止,还要看其是否 ... 有力的工具,只要你笑得真诚,笑得和善就能叩开他人的心扉,达到相互信任的目的。",
+    "title" :"最新社交礼仪实用手册:  - Google 圖書結果",
+    "metadata" :"王峰 - 2016",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=I64ODgAAQBAJ&pg=PT124&lpg=PT124&dq=%E4%BF%A1%E4%BB%BB+%E4%B8%8E%E7%A4%BC%E4%BB%AA&source=bl&ots=m7JV1QBdlW&sig=uQ82gIXwn7AdbM68v-aiqi3OPno&hl=zh-TW&sa=X&ved=0ahUKEwjV8ZPmq4HUAhVGKZQKHXvDDHUQ6AEISzAJ",
+    "abstract" :"因此,对于刚刚步入社会的年轻人来讲,学习必要的礼仪知识是十分重要的。 ... 在言行举止之间表现出友善和真诚,别人才愿意和你交往,你才会赢得对方的好感和信任。",
+    "title" :"二十几岁要懂的100条人生经验:  - Google 圖書結果",
+    "metadata" :"马银文 - 2017",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=RGyYCwAAQBAJ&pg=PT118&lpg=PT118&dq=%E4%BF%A1%E4%BB%BB+%E4%B8%8E%E7%A4%BC%E4%BB%AA&source=bl&ots=mXset-98fP&sig=5h0IkbuxzP_ZjYjRuLKM04pqSLc&hl=zh-TW&sa=X&ved=0ahUKEwjV8ZPmq4HUAhVGKZQKHXvDDHUQ6AEIUDAK",
+    "abstract" :"学习礼仪知识,有助于个人成长通晓礼仪知识,一个人才会在社会中立于不败之地;学习 ... 之间表现出友善和真诚,别人才愿意和你交往,你才会赢得对方的好感和信任。",
+    "title" :"二十几岁要明白的108条人生经验:  - Google 圖書結果",
+    "metadata" :"王峰 - 2016",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=JsBMBAAAQBAJ&pg=PT41&lpg=PT41&dq=%E4%BF%A1%E4%BB%BB+%E4%B8%8E%E7%A4%BC%E4%BB%AA&source=bl&ots=6VA505y4an&sig=E0mowR0bpe0k06Zi3bg0gny4rnc&hl=zh-TW&sa=X&ved=0ahUKEwjV8ZPmq4HUAhVGKZQKHXvDDHUQ6AEIVTAL",
+    "abstract" :"一句“像我这样的人”,往往能消除别人对我们的不信任感在有着众多的学者及评论家的座谈会中,主办单位安排了一位家庭主妇列席发言。当轮到她发言时,她居然用“像 ...",
+    "title" :"经理人必备商务社交与礼仪知识:  - Google 圖書結果",
+    "metadata" :"赵涛 许进 - 2014",
+    },
+    {
+    "url" :"https://www.ptt.cc/man/Kaohsiung/DF55/D110/M.1253671004.A.126.html",
+    "abstract" :"標題[問題] 能信任的禮儀公司. 時間Tue Sep 22 14:36:21 2009. 這問題有點嚴肅~不想認真回答的煩請左轉出去, 朋友的母親目前在榮總的安寧病房彌留, 因友人是 ...",
+    "title" :"[問題] 能信任的禮儀公司- 精華區Kaohsiung - 批踢踢實業坊",
+    },
+    {
+    "url" :"http://xinli.9939.com/rjjw/jjqt/2010/0108/827693.shtml",
+    "abstract" :"2010年1月8日 - 如：向前走，迈台阶跨上台阶向右拐然后交换角色，B闭眼，A指引B走路○ 游戏说明： 通过亲身体验，让学员体会信任与被信任的感觉作.",
+    "title" :"趣味游戏：建立信任关系_人际交往礼仪人际交往禁忌_交际趣谈_久久 ...",
+    },
+    ],
+######
+#Q_1_0 #:信任 与义气
+######
+"Q_1_0": ["信任 与义气",
+    {
+    "url" :"https://kknews.cc/zh-hk/entertainment/v2ob6a.html",
+    "abstract" :"Will娛樂相關人員表示：「我方將和真理翰繼續共事，延續過去三年互相累積的信任。很高興真理翰對我公司展現如此的信任與義氣，往後我方也會不吝繼續給予支援， ...",
+    "title" :"真理翰續約老東家展信任與義氣- 每日頭條",
+    },
+    {
+    "url" :"http://www.loldk.com/fm/f/a/aview-3410.html",
+    "abstract" :"Faker坚守信任与义气，与SKT续约继续征战S6. 11-17 阅读(81万+) 职业联赛 来源：OSEN，翻译：YaYa. 如今大家应该要从Faker李相赫离开SKT的梦中醒来了，Faker ...",
+    "title" :"Faker坚守信任与义气，与SKT续约继续征战S6_LOL资讯_LOL大咖",
+    },
+    {
+    "url" :"http://www.allthatstar.com/view.php?action=304&postID=262162",
+    "abstract" :"2016年7月26日 - Will娱乐相关人员表示：“我方将和真理翰继续共事，延续过去三年互相累积的信任。很高兴真理翰对我公司展现如此的信任与义气，往后我方也会不吝 ...",
+    "title" :"真理翰续约老东家展信任与义气 - 全星网",
+    },
+    {
+    "url" :"http://m.allthatstar.com/tw/view.php?action=5205&postID=262153",
+    "abstract" :"2016年7月26日 - Will娛樂相關人員表示：「我方將和真理翰繼續共事，延續過去三年互相累積的信任。很高興真理翰對我公司展現如此的信任與義氣，往後我方也會...",
+    "title" :"真理翰續約老東家展信任與義氣 - 全星网",
+    },
+    {
+    "url" :"https://tw.news.yahoo.com/%E7%94%B3%E5%BD%97%E6%98%9F%E7%BA%8C%E7%B4%84%E8%80%81%E6%9D%B1%E5%AE%B6-%E5%B1%95%E9%95%B7%E5%B9%B4%E4%BF%A1%E4%BB%BB%E8%88%87%E7%BE%A9%E6%B0%A3-235611766.html",
+    "abstract" :"2016年7月6日 - 基於兩人長時間累積下來的信賴與義氣，申彗星再一次和LIVEWORKS續約，並不收任何合約金。 申彗星表示：「在進行演藝活動時，我認為最重要的 ...",
+    "title" :"申彗星續約老東家展長年信任與義氣 - 奇摩新聞",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=r0jhPktVrgcC&pg=PA317&lpg=PA317&dq=%E4%BF%A1%E4%BB%BB+%E4%B8%8E%E4%B9%89%E6%B0%94&source=bl&ots=bgwsmoe083&sig=aifsgtIPD9gdIeJYT8A2YDFZsFQ&hl=zh-TW&sa=X&ved=0ahUKEwi23P_eq4HUAhVDH5QKHSMwC74Q6AEIUzAL",
+    "abstract" :"信任雖然抽象,但信任的表現要非常具體。鴻海在 2004 年接管 ... 關羽是山西人,也是個武財神,非常講義氣;郭台銘覺得信任從某些角度來講,也是義氣。當年曹操兵敗走 ...",
+    "title" :"鴻海藍圖(精):  - 第 317 頁 - Google 圖書結果",
+    "metadata" :"伍忠賢 - 2009 - Business & Economics",
+    },
+    {
+    "url" :"https://tw.mobi.yahoo.com/home/%E7%94%B3%E5%BD%97%E6%98%9F%E7%BA%8C%E7%B4%84%E8%80%81%E6%9D%B1%E5%AE%B6-%E5%B1%95%E9%95%B7%E5%B9%B4%E4%BF%A1%E4%BB%BB%E8%88%87%E7%BE%A9%E6%B0%A3-235611766.html",
+    "abstract" :"2016年7月7日 - 7月7日，經紀公司LIVEWORKS表示已和旗下歌手申彗星續約。本次申彗星的續約是繼2009年和LIVEWORKS簽約後第三次的續約，申彗星 ...",
+    "title" :"申彗星續約老東家展長年信任與義氣",
+    },
+    {
+    "url" :"https://www.facebook.com/jamsclub/posts/10156290116820646",
+    "abstract" :"我本人生性很講義氣，現在被信任過的人出賣，我相信講義氣的人很能感受其受，被自己相信的人出賣，死而無言。",
+    "title" :"我本人生性很講義氣，現在被信任過的人出賣，我相信講義氣 ... - Facebook",
+    },
+    {
+    "url" :"http://ent.sina.com.cn/m/c/2017-03-28/doc-ifycstww1459085.shtml",
+    "abstract" :"2017年3月28日 - 苏有朋向每位主演和工作人员表达了感谢：林心如在最幸福的时候义气地来支持这部电影。 新浪娱乐讯苏有朋[微博]担任导演的第二部电影《嫌疑人x ...",
+    "title" :"苏有朋大赞林心如义气感谢王凯张鲁一信任 - 新浪娱乐",
+    },
+    ],
+######
+#Q_1_3 #:信任的本质及其文化
+######
+"Q_1_3": ["信任的本质及其文化",
+    {
+    "url" :"http://www.aisixiang.com/data/102325.html",
+    "abstract" :"2016年11月29日 - 其划分原因源自中西文化对人性及其社会依赖性的不同假定以及它们所构成的关系网络偏向或制度性偏向由此，信任的本质是社会成员在面对社会 ...",
+    "title" :"翟学伟：信任的本质及其文化_爱思想",
+    },
+    {
+    "url" :"http://www.society.shu.edu.cn/CN/abstract/abstract13915.shtml",
+    "abstract" :"摘要 随着信任的研究越来越受重视，信任的含义也在不同的学科领域中越来越复杂。如果回到常理及其文化中来讨论信任，它所展现出来的特征可分为无约束机制的 ...",
+    "title" :"信任的本质及其文化 - 社会杂志 - 上海大学",
+    "metadata" :"由 翟学伟 著作 - 被引用 19 次 - 相關文章",
+    },
+    {
+    "url" :"http://www.mzb.com.cn/html/report/140720634-1.htm",
+    "abstract" :"2014年7月2日 - 其划分原因源自中西文化对人性及其社会依赖性的不同假定以及它们所构成的关系网络偏向或制度性偏向。由此，信任的本质是社会成员在面对社会 ...",
+    "title" :"信任的本质及其文化- 中国民族宗教网",
+    },
+    {
+    "url" :"https://read01.com/JPGJz5.html",
+    "abstract" :"2014年7月2日 - 隨著信任的研究越來越受重視，信任的含義也在不同的學科領域中越來越複雜。如果回到常理及其文化中來討論信任，它所展現出來的特徵可分為無 ...",
+    "title" :"翟學偉：信任的本質及其文化- 壹讀",
+    },
+    {
+    "url" :"http://www.jianshu.com/p/837ba9f718fc",
+    "abstract" :"2015年10月22日 - 阅读时间:下午三点半到六点阅读地点:寝室阅读方式:精读《信任的本质及其文化》这篇文章由南京大学社会学院心理学系的翟学伟老师所写，为国家社 ...",
+    "title" :"对《信任的本质及其文化》的阅读小结- 简书",
+    },
+    {
+    "url" :"http://www.looooker.com/archives/13574",
+    "abstract" :"2015年5月17日 - 信任的本质及其文化【作者】 翟学伟； 【Author】 ZHAI Xuewei;Department of Psychology,School of Social and Behavioral Sciences,Nanjing ...",
+    "title" :"#研究分享#【信任的本质及其文化】 – 北大新媒体",
+    },
+    {
+    "url" :"http://www.360doc.com/relevant/390361490_more.shtml",
+    "abstract" :"非正式约束与经济绩效。制度由正式规则、非正式约束和实施机制的有效性三部分构成。与康芒斯等许多制度经济学家不同，诺思十分强调非正式约束的作用，他认为非 ...",
+    "title" :"翟学伟--信任的本质及其文化--相关文章 - 360doc",
+    },
+    {
+    "url" :"http://www.wxrw123.com/zj/20160412/173229.html",
+    "abstract" :"2016年4月12日 - 2015年5月9日,南京大学社会学系翟学伟教授在博学楼110教室为东北财经大学师生做了题为《信任的本质及其文化》的学术讲座。这是2015年社会与 ...",
+    "title" :"翟学伟《信任的本质及其文化》学术报告结束_微信热文",
+    },
+    {
+    "url" :"https://www.zhihu.com/question/25907782",
+    "abstract" :"2014年10月12日 - 翟学伟老师在做一个关于社会信用的项目，在《社会》上发过了一篇《信任的本质及其文化》可供参考。 另外除去上面的老师已经说过的“知道做什么” ...",
+    "title" :"信任信任机制（社会科学方向） 信息不对称。关于这几个关键词求推荐的 ...",
+    },
+    {
+    "url" :"https://books.google.com.hk/books?id=jOuTAgAAQBAJ&pg=PA67&lpg=PA67&dq=%E4%BF%A1%E4%BB%BB%E7%9A%84%E6%9C%AC%E8%B4%A8%E5%8F%8A%E5%85%B6%E6%96%87%E5%8C%96&source=bl&ots=YEvgfy9_m8&sig=71UsXuT72y8C_xx2VtxGb8-5KgY&hl=zh-TW&sa=X&ved=0ahUKEwjL-ejtq4HUAhVGJJQKHVsUBasQ6AEIajAJ",
+    "abstract" :"此種「關係」的權力結構在本質上是垂直的、不對稱的。 ... 這些文獸常常引用這諸如信任、可靠和互惠之類的關係網絡及其文化成分'來解釋東南亞華人商業的快速發展。",
+    "title" :"華商：族裔資源與商業謀略:  - 第 67 頁 - Google 圖書結果",
+    "metadata" :"陳國賁 - 2010 - Social Science",
+    },
+    ],
+######
+#Q_1_2 #:如何快速建立信任
+######
+"Q_1_2": ["如何快速建立信任",
+    {
+    "url" :"http://angellin0522.pixnet.net/blog/post/168564894-%E5%BF%AB%E9%80%9F%E5%BB%BA%E7%AB%8B%E4%BF%A1%E4%BB%BB%E6%84%9F%E7%9A%84%E7%B5%82%E6%A5%B5%E7%A7%98%E8%A8%A3",
+    "abstract" :"我個人覺得在業務銷售上最挑戰的地方是開發新客戶， 而開發新客戶最大挑戰就是與客戶建立信任感。 所以，如何與客戶建立信任感是每個人業務人員必學的學分。",
+    "title" :"快速建立信任感的終極秘訣@ 林哲安的部落格(暢銷書業務九把刀作者 ...",
+    },
+    {
+    "url" :"https://www.youtube.com/watch?v=1qh4lR3bEBM",
+    "abstract" :"免費電子書下載：http://bit.ly/client-action 官方網站：http://400million.com.tw/ FB學習社團：http://bit.ly/4m-fb-groups.",
+    "title" :"如何快速建立信任感？（有溫度的行銷才是關鍵！） - YouTube",
+    "metadata" :"2016年2月1日 - 上傳者：FB許豪 行銷補給讚",
+    },
+    {
+    "url" :"http://zh.wikihow.com/%E5%BB%BA%E7%AB%8B%E4%BF%A1%E4%BB%BB",
+    "abstract" :"如何建立信任. 取得他人的信任对于成功的人际关系来说特别重要。虽然失去别人信任是常有的事，但只要你肯付出努力，好好地准备，就能建立别人的信任。如果你的 ...",
+    "title" :"5种方法来建立信任 - wikiHow",
+    },
+    {
+    "url" :"https://kknews.cc/zh-hk/news/jm4qmq.html",
+    "abstract" :"2016年7月30日 - 【問題】雨總您好，我是平安公司主管，平安公司現在做全面綜合金融業務，我可以做存儲款，貸款。信用卡等銀行業務，也做所有保險業務，同時，醫療 ...",
+    "title" :"金融保險銷售，陌生客戶如何快速建立信任（頂級高手銷售秘訣） - 每日頭條",
+    },
+    {
+    "url" :"http://blog.terryfu.com/archives/1164",
+    "abstract" :"2010年8月5日 - ... 第一次和我媽媽見面，短短不到兩小時，就讓我媽媽非常信任他！洽談的整個過程，其實就是運用一個極簡單方法來快速建立客戶信任感的示範...",
+    "title" :"快速增加客戶信任感的秘訣：第一個提供資訊！ - 網路行銷第一站！",
+    },
+    {
+    "url" :"http://blog.xuite.net/wuyunhua333/twblog/134897344-%E8%AE%93%E5%AE%A2%E6%88%B6%E6%8E%8F%E9%8C%A2%E5%9F%8B%E5%96%AE%E7%9A%84%E5%BF%85%E6%AE%BA%E5%85%AD%E9%83%A8%E6%9B%B2...%E5%BB%BA%E7%AB%8B%E4%BF%A1%E4%BB%BB%E6%84%9F",
+    "abstract" :"快速搏取客戶的好感，目的當然是要建立信任感。只要是業務老手都知道，客戶沒有信任感，成交就不可能有效率。永達保險經紀人的超級業務員李秋蓮，對此有深刻的 ...",
+    "title" :"讓客戶掏錢埋單的必殺六部曲...建立信任感@ 鞋鞋唸的雙魚座:: 隨意窩 ...",
+    },
+    {
+    "url" :"http://blogs.bnet.com.cn/?action-viewspace-itemid-26457",
+    "abstract" :"2013年2月25日 - 在一个高度竞争的商业社会，信任是一切商业活动的基础。如果在员工、合作伙伴、或者客户之间，我们不能建立真正的信任，那么我们不可能实现 ...",
+    "title" :"5个方法快速建立信任- 天天向上的个人空间- BNET商学院- powered by ...",
+    },
+    {
+    "url" :"http://mt.sohu.com/business/d20170517/141062726_426655.shtml",
+    "abstract" :"保险行业，陌生客户如何快速建立信任关系？ 2017-05-16 09:23. 【背景】：. 1、我是平安公司主管，平安公司现在做全面综合金融业务，我可以做存储款，贷款。信用卡 ...",
+    "title" :"保险行业，陌生客户如何快速建立信任关系？ - 搜狐公众平台",
+    },
+    {
+    "url" :"http://www.soundofhope.org/b5/2016/09/18/n480708.html",
+    "abstract" :"2016年9月18日 - 信任感的建立除了外在形象，內在行為舉止的內涵及表現是增加信任感及信賴度重要的一環.",
+    "title" :"【悠活-談心室】如何快速建立信任感—談內在行為舉止的建立第28集(音頻 ...",
+    },
+    {
+    "url" :"http://weibo.com/ttarticle/p/show?id=2309404036668930834642",
+    "abstract" :"2016年10月31日 - 昨天把以前学校的学生拉了个群，自14年9月11日离开学校工作到现在也有2年了，先去了潮州后来了北京，时间飞快。。。学生毕业工作短的也有个2 ...",
+    "title" :"销售如何快速建立信任 - 微博台灣站",
+    },
+    ],
 }
 keyword_list = [ 
-   "信任 与义气",
-   "信任 与礼仪",
-   "如何快速建立信任",
-   "信任的本质及其文化",
+   "   秩",
+   "   可视化",
+   "   4因5路",
+   "   计算复杂度:常，对，线，平，立，指，阶",
+   "   意义",
+   "   概率",
+   "   演化",
+   "   balance shift",
+   "   证伪",
+   "   短缺和过剩",
+   "   交换",
+   "   外延",
+   "   统一",
+   "   健康",
+   "   展示",
+   "   有用",
+   "   边界",
+   "   未来",
+   "   不确定",
+   "   分层模块化",
+   "   不动点",
+   "   分块",
+   "   抽象",
+   "   局部化",
+   "   如何",
 ]
-meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+meta_data = {"status":".","Major": 1,"Minor": 4,"Prefix": "Q"}
 km_0_title = "信任"
 km_0_summary = "ummary_tp"
-km_1_tags = ["期望","喜欢","礼仪"]
+km_1_tags = ["
