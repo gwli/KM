eff7dd380aca0d7f89f5a87aff996da8670f492d
victor <gwli@outlook.com>
2017-05-23 09:48:50 +0800

    冬天

---
+++ source/Li/Li.py
@@ -0,0 +1,17 @@
+
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "礼 与 理",
+   "礼 的本质是什么",
+   "礼 有哪些种",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "礼仪"
+km_0_summary = "礼仪"
+km_1_tags = []
---
+++ source/Winter/Winter.py
@@ -0,0 +1,20 @@
+
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "人生的困境有哪些",
+   "困境的意义 ",
+   "困境的本质",
+   "困境的破局",
+   "困境的周期",
+   "心理学对 困境的解读",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "冬天怎么过"
+km_0_summary = "冬天怎么过"
+km_1_tags = []
