f24f6be1d8148d822f6d1f1471b6821581ae9744
victor <gwli@outlook.com>
2017-05-24 08:27:29 +0800

    礼的意义

--- source/Li/Li.py
+++ source/Li/Li.py
@@ -6,11 +6,6 @@ Q_dict = {
 ######
 "Q_1_1": ["礼 的本质是什么",
     {
-    "url" :"http://www.baike.com/wiki/%E7%A4%BC",
-    "abstract" :"礼是中国传统社会道德规范的体现，同时也是古代社会，尤其是封建社会政治制度 ... 1正文; 2礼的介绍; 3礼的本质; 4礼的作用; 5以礼入法; 6《辞海》中的解释; 7典籍中 ...",
-    "title" :"礼_互动百科",
-    },
-    {
     "url" :"http://www.dizigui.cn/bk/lyls151231.asp",
     "abstract" :"但是唯有一个东西它是亘古不变、超越时空，那就是礼的本质。我们今天学就是要学礼的本质，礼的本质是什么？礼的本质一个字，我们要牢牢记住这一个字，“敬”。",
     "title" :"礼的本质是敬-弟子规公益网",
@@ -21,12 +16,6 @@ Q_dict = {
     "title" :"礼仪的本质是什么？ - 知乎",
     },
     {
-    "url" :"http://www.eastasia.ntu.edu.tw/member/eastasia/temp/7-2/7-2-12.pdf",
-    "abstract" :"體驗、知識、意願）的尺度，在禮、仁關係中，不僅有仁體現禮的本質. （「人而不仁如禮 .... 成？它的意義是什麼？孔子的禮觀念與此前就已經形成的觀念之間是什麼.",
-    "title" :"[PDF] 析論《論語》中禮與仁的關係A Disscusion of the Relationship between ...",
-    "metadata" :"由 顏世安 著作 - 被引用 1 次 - 相關文章",
-    },
-    {
     "url" :"http://zhidao.baidu.com/question/751380372365072524.html",
     "abstract" :"2013年11月15日 - 礼的本质是什么？在先贤看来，礼是可以用来守国家、行政令、不失民的（礼,经国家,定社稷,序民人,利后嗣者也）。那么礼有什么特点，以至于可以用礼 ...",
     "title" :"礼的本质是什么？_百度知道",
@@ -35,26 +24,6 @@ Q_dict = {
     "url" :"http://theory.gmw.cn/2015-09/14/content_17035825.htm",
     "abstract" :"2015年9月14日 - 礼物”、“送礼”与“礼”的关系是什么？ 彭林：中国是礼仪之邦，离开了“礼”就不能成其为礼仪之邦。 中国古代的统治者很早就明白，要想长治久安就要 ...",
     "title" :"“礼”的本质是尊重(1)_理论综合_光明网",
-    },
-    {
-    "url" :"http://www.huanghuagang.org/hhgMagazine/issue07/big5/34.htm",
-    "abstract" :"全閥時代」一詞比「文革時代」一詞，更精確的反映了那個時代的本質特徵。 ... 因此可見，理論只能是相對的，即使禮制的復興這樣激動人心的事也不是什麼永恆的事業， ...",
-    "title" :"論「禮」九章（上篇）",
-    },
-    {
-    "url" :"http://www.360doc.com/content/14/1006/14/739691_414763233.shtml",
-    "abstract" :"2014年10月6日 - 鲁国人林放也曾问过孔子关于礼的本质问题。 【论语】林放问礼之本。子曰:“大哉问!礼,与其奢也,宁俭；丧,与其易也,宁戚。”(八佾4) 【译文】林放问什么是 ...",
-    "title" :"什么是礼 - 360doc",
-    },
-    {
-    "url" :"http://www.jianshu.com/p/a4ba34e87017",
-    "abstract" :"2017年4月24日 - 2017-04-21 翻腾四海四海书斋在马克思的理论中，凡是一个重要概念，都会有着极为丰富的内涵。他的某一个概念，解释到最后，都要说一说它的“ ...",
-    "title" :"58、孔子所宣扬的“礼”的本质是什么：论阶级存在的必然性- 简书",
-    },
-    {
-    "url" :"http://www.ccdi.gov.cn/lt/qfwy/wyzd/201609/t20160920_87054.html",
-    "abstract" :"2016年9月21日 - 鲁人林放问礼的本质是什么，孔子对这个问题，表示了极大的兴趣，他回答说，“就一般的礼仪说，与其铺张浪费，不如朴素俭约；就丧礼说，与其追求周 ...",
-    "title" :"【微也足道】十句话读懂孔子的礼 - 中央纪委监察部网站",
     },
     ],
 ######
@@ -67,51 +36,14 @@ Q_dict = {
     "title" :"古礼今用-礼与理 - 中华礼仪",
     },
     {
-    "url" :"https://kknews.cc/zh-hk/news/l8obv9e.html",
-    "abstract" :"2016年4月25日 - 朱熹主動追求禮儀化的人生，因其特重禮教，所以長久以來，就社會生活層面而言，人們認為朱子學就是禮學。朱熹最核心的思想「理」既根源於對禮的 ...",
-    "title" :"朱熹人生中的「禮」與「理」 - 每日頭條",
-    },
-    {
-    "url" :"http://www.360doc.com/content/16/0426/10/30119791_553878315.shtml",
-    "abstract" :"2016年4月26日 - 朱熹（1130-1200）是中国思想学术史上的“大人物”，其学术思想体现出“致广大、尽精微、综罗百代”的特点。朱熹一方面为新儒学建立了一个完整、 ...",
-    "title" :"朱熹人生中的“礼”与“理”(图) - 360doc",
-    },
-    {
-    "url" :"https://books.google.com.hk/books?id=m0h089HiVckC&pg=PA255&lpg=PA255&dq=%E7%A4%BC+%E4%B8%8E+%E7%90%86&source=bl&ots=TeRX6vmd1s&sig=8eCf-61j1JZZRlsS-kQosqs56cs&hl=zh-TW&sa=X&ved=0ahUKEwi0iYaP-ITUAhUMjZQKHfygCXYQ6AEINzAD",
-    "abstract" :"... 也難把握得多 0 據《禮言己‧樂記》的說法 0 「禮也者 0 理之不可易者也」 0 這是把禮與理看作一事。而據《禮記‧禮運》 0 「禮也者 0 義之實也 0 協諸義而協 0 則體雖先王 ...",
-    "title" :"尋求自然秩序中的和諧: 中國傳統法律文化研究",
-    "metadata" :"梁治平 - 2011 - Culture and law",
-    },
-    {
-    "url" :"http://crchat.crc.com.cn/2008/200803/content4.htm",
-    "abstract" :"走过了半壁风雨、半壁阳光，华润的故事，神秘而又曲折；前行的路上，甜蜜夹杂着艰辛，然而，她的无私和聪慧，却为故事留下了许多“礼”与“理”的“传说”……她所创造的 ...",
-    "title" :"《华润》杂志---------70年“礼”与“理”",
-    },
-    {
     "url" :"http://www.kaiwind.com/culture/hot/201601/16/t20160116_3378142.shtml",
     "abstract" :"2016年1月22日 - 中国传统的“礼”与“理”到底有着怎样的关联与区别？从礼学到理学的转化中，理学把礼学的很多概念观点进一步深化了。原标题：礼学与理学《光明 ...",
     "title" :"“礼”与“理”到底有着怎样的关联与区别_热点_凯风网",
     },
     {
-    "url" :"https://ir.nknu.edu.tw/ir/retrieve/18685/%E5%BC%B5%E8%BC%89%E5%93%B2%E5%AD%B8%E4%B8%AD%E7%9A%84%E3%80%8C%E7%90%86%E3%80%8D%E8%88%87%E3%80%8C%E7%A6%AE%E3%80%8D.pdf",
-    "abstract" :"本文從以下三方面探討張載思想中的「理」與「禮」。首先，本文將討. 論張載對禮之根源的探尋，並指出在張載思想中，天理流行是禮的形上根源，. 而禮是天理的具體呈現 ...",
-    "title" :"[PDF] 張載哲學中的「理」與「禮」 - 高雄師範大學",
-    "metadata" :"由 陳政揚 著作 - 2005 - 被引用 4 次 - 相關文章",
-    },
-    {
-    "url" :"http://big.hi138.com/wenxueyishu/dangdaiwenxue/200901/69329.asp",
-    "abstract" :"2009年1月12日 - 摘要：《西廂记》中的崔鶯鶯與《牡丹亭》中的杜麗娘都是沖破封建束縛，追求自由愛情的抗爭形象。但二者的反抗對象有所不同：崔鶯鶯的反叛行為是对 ...",
-    "title" :"“禮”與“理”的叛逆_現當代文學論文_免費論文下載中心",
-    },
-    {
     "url" :"https://www.douban.com/note/376247534/",
     "abstract" :"2014年7月20日 - 礼”除了形式上的客套外，在一定的场合，还有还有物质上的馈赠，这种馈赠也是“礼”的重要组成部分，它们共同反映出了人与人之间交往的道“理”，也 ...",
     "title" :"“礼”与“理” - 豆瓣",
-    },
-    {
-    "url" :"https://www.douban.com/group/topic/92099203/",
-    "abstract" :"来自: 花和尚(流连忘返) 2016-10-20 03:47:47. 读了这么多年陈嘉映，忽而有一天想到，老陈对“理”的态度，跟夫子对“礼”的态度，有几分神似。 不知各位有什么感想？",
-    "title" :"理与礼 - 豆瓣",
     },
     ],
 ######
@@ -122,11 +54,6 @@ Q_dict = {
     "url" :"https://kknews.cc/zh-hk/culture/3oeomy.html",
     "abstract" :"中國的古代的禮儀分類大致可以分為五種。 第一種、吉禮。 吉禮是五禮之冠，主要是對天神、地祗、人鬼的祭祀典禮。 吉禮的主要內容有祀天神、祭地祗以及祭人鬼。",
     "title" :"禮儀之邦——中國古代的禮儀有哪些？ - 每日頭條",
-    },
-    {
-    "url" :"https://zh.wikipedia.org/zh-hk/%E8%81%96%E7%A6%AE",
-    "abstract" :"聖事（天主教汉译，拉丁语：Sacramentum）或聖禮（新教汉译）是基督教傳達神聖恩典的儀式。東正教與之對應的名稱是奧蹟（Holy Mysteries）。 羅馬天主教會、東正教、 ...",
-    "title" :"聖禮- 维基百科，自由的百科全书",
     },
     {
     "url" :"https://books.google.com.hk/books?id=_xzDCwAAQBAJ&pg=PT377&lpg=PT377&dq=%E7%A4%BC+%E6%9C%89%E5%93%AA%E4%BA%9B%E7%A7%8D&source=bl&ots=abiTPxxIjJ&sig=pmktWLPuw9hwyNYIVzdeCjcWF9U&hl=zh-TW&sa=X&ved=0ahUKEwil9Juf-ITUAhVDJpQKHY_cAN8Q6AEILTAC",
@@ -140,27 +67,9 @@ Q_dict = {
     "title" :"聖餐禮- 维基百科，自由的百科全书",
     },
     {
-    "url" :"https://books.google.com.hk/books?id=d8k-8cFAZBMC&pg=PA90&lpg=PA90&dq=%E7%A4%BC+%E6%9C%89%E5%93%AA%E4%BA%9B%E7%A7%8D&source=bl&ots=XLMT7RglgI&sig=_MIu7D16TvZrSTv8z4f8zTwmQVs&hl=zh-TW&sa=X&ved=0ahUKEwil9Juf-ITUAhVDJpQKHY_cAN8Q6AEIODAE",
-    "abstract" :"這些井、澤、淵可能對某種疾病具有控制的神力,因而成為祝由巫術的必要道具。 ... 《周禮,大宗伯》:「以埋、沉祭山林、川澤。 ... 然則,楚人祭水神所沉的祭物有哪些呢?",
-    "title" :"新出簡帛與禮制研究 - 第 90 頁 - Google 圖書結果",
-    "metadata" :"2007 - China",
-    },
-    {
     "url" :"https://www.zhihu.com/question/43655791",
     "abstract" :"2016年8月5日 - 从之前参加婚礼的体验来说，大部分的伴手礼都没有留下什么深刻印象，多是比较粗糙的情侣物件…觉得能让人眼前一亮的，可能应该是本身很特别 ...",
     "title" :"有哪些让人眼前一亮的婚礼伴手礼？ - 知乎",
-    },
-    {
-    "url" :"https://books.google.com.hk/books?id=cgKr_MtVmv8C&pg=PA15&lpg=PA15&dq=%E7%A4%BC+%E6%9C%89%E5%93%AA%E4%BA%9B%E7%A7%8D&source=bl&ots=6Och2PzdRq&sig=mP_wl6x8HguytuFpC680DTy5M0Q&hl=zh-TW&sa=X&ved=0ahUKEwil9Juf-ITUAhVDJpQKHY_cAN8Q6AEIRDAG",
-    "abstract" :"市场调查的功能和作用有哪些? ... 市场研究的基本方法有哪些? ... 案例分析案例 1 - 1 美国礼维公司的分类市场调查美国礼维公司是以生产牛仔裤而闻名世界的。",
-    "title" :"市场调查与预测 - 第 15 頁 - Google 圖書結果",
-    "metadata" :"龚曙明 - 2005 - Marketing research",
-    },
-    {
-    "url" :"https://books.google.com.hk/books?id=bh3DCwAAQBAJ&pg=PT149&lpg=PT149&dq=%E7%A4%BC+%E6%9C%89%E5%93%AA%E4%BA%9B%E7%A7%8D&source=bl&ots=iGUgi5rvE4&sig=Cbj6RQ860H9xNWP2oUvaM984WnE&hl=zh-TW&sa=X&ved=0ahUKEwil9Juf-ITUAhVDJpQKHY_cAN8Q6AEISTAH",
-    "abstract" :"礼包括哪些内容礼是古代的圣贤们禀承天理之规则所制定,用以治理“人之情”。《礼记∙礼运》 ... 但是这种礼的社会制度是源于天道,合乎人情常理,是一种人性本善的管理。",
-    "title" :"国学修养基本知识（读酷国学百科版） - Google 圖書結果",
-    "metadata" :"林语涵 - 2015 - Literary Criticism",
     },
     {
     "url" :"https://books.google.com.hk/books?id=o-_UCQAAQBAJ&pg=PT164&lpg=PT164&dq=%E7%A4%BC+%E6%9C%89%E5%93%AA%E4%BA%9B%E7%A7%8D&source=bl&ots=Og-fasRRbX&sig=wZK8R2txHO-Ar99-KTnwrbDBMgY&hl=zh-TW&sa=X&ved=0ahUKEwil9Juf-ITUAhVDJpQKHY_cAN8Q6AEITjAI",
@@ -177,12 +86,13 @@ Q_dict = {
     ],
 }
 keyword_list = [ 
-   "   秩",
-   "   可视化",
-   "   4因5路",
-   "   排列组合",
-   "   边际效应",
-   "   费米计算",
+   " 路上遇到一堆领导，如何打招呼 ",
+   " 礼与道德",
+   " 礼与罚",
+   " 礼 与乐的关系",
+   " 如何定义 人与人之间的距离",
+  "儒家礼仪分为吉、凶、宾、军、嘉五种，合称“五礼",
+   "“四书五经”和“十三经”的形成。",
    "   演化",
    "   从心开始",
    "   批判",
@@ -203,7 +113,7 @@ keyword_list = [
    "   很感谢你能能来，不遗憾你离开",
    "   是什么",
 ]
-meta_data = {"status":".","Major": 1,"Minor": 3,"Prefix": "Q"}
+meta_data = {"status":"mc","Major": 1,"Minor": 3,"Prefix": "Q"}
 km_0_title = "礼仪"
 km_0_summary = "礼仪"
 km_1_tags = []
