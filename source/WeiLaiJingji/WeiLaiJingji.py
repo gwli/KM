af17a436e26e2f5bb22154a0fd558c055600fe47
victor <gwli@outlook.com>
2017-05-25 22:17:34 +0800

    未来经济

---
+++ source/WeiLaiJingji/WeiLaiJingji.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "未来经济 的发展模型",
+   "未来经济 突破点",
+   "未来科技 突破路线",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "title_tp"
+km_0_summary = "ummary_tp"
+km_1_tags = []
