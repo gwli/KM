f95d9248c1d76c22c57f8933c62dd2036b09002e
victor <gwli@outlook.com>
2017-06-01 09:31:30 +0800

    添加 时尚与健康

---
+++ source/WeiLaiJingji/ChengXuYuan.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+}
+keyword_list = [ 
+   "未来的经济发展模式",
+   "数字经济的3.0模型",
+   "工业4.0",
+   "新能源技术有哪些",
+   "新材料技术有哪些",
+]
+meta_data = {"status":"mc","Major": 1,"Minor": 3,"Prefix": "Q"}
+km_0_title = "程序员"
+km_0_summary = "ummary_tp"
+km_1_tags = []
---
+++ source/WeiLaiJingji/SportAndHealth.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   " 运动健康的发展趋势",
+   " 一般人如何练出特种兵的水平",
+   " 运动如何练",
+   " 运动从哪里开始",
+   " 是不是从跑步开始，从力量训练开始",
+   " 如何自我练习成为一个特种兵",
+]
+meta_data = {"status":".","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "运动"
+km_0_summary = "ummary_tp"
+km_1_tags = []
