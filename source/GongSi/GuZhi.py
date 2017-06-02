a96c3c4260d7a077de7f67bcb061233235603431
victor <gwli@outlook.com>
2017-06-02 09:59:44 +0800

    趋势的初步整理

---
+++ source/GongSi/GuZhi.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "估值，现值，与终值",
+   "公司如何估值",
+   "公司增长 模型预测",
+   "如何彻底弄清楚一个行业或者一家公司实际上是如何经营运作的？",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "公司估值"
+km_0_summary = "ummary_tp"
+km_1_tags = []
---
+++ source/ShiMoXi/ShiMoXi.py
@@ -0,0 +1,19 @@
+
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "石墨稀 相关的公司有哪些",
+   "石墨稀 发展现状",
+   "石墨稀   本质",
+   "石墨稀   最有可能的应用",
+   "石墨稀 替代品",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "石墨稀"
+km_0_summary = "ummary_tp"
+km_1_tags = []
