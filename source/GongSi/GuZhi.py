0399ff43e310b072344c6d7d55c1e28b5e515b28
victor <gwli@outlook.com>
2017-06-05 10:37:51 +0800

    估值

--- source/GongSi/GuZhi.py
+++ source/GongSi/GuZhi.py
@@ -6,11 +6,6 @@ Q_dict = {
 ######
 "Q_1_1": ["公司如何估值",
     {
-    "url" :"http://baike.baidu.com/item/%E5%85%AC%E5%8F%B8%E4%BC%B0%E5%80%BC",
-    "abstract" :"公司估值，又名企业估值、企业价值评估等。公司估值是指着眼于上市或非上市公司本身，对其内在价值进行评估。一般来讲，公司的资产及获利能力决定于其内在价值 ...",
-    "title" :"公司估值_百度百科",
-    },
-    {
     "url" :"http://wiki.mbalib.com/wiki/%E5%85%AC%E5%8F%B8%E4%BC%B0%E5%80%BC",
     "abstract" :"公司估值是投融资、交易的前提。一家投资机构将一笔资金注入企业，应该占有的权益首先取决于企业的价值。而一个成长中的企业值多少钱？这是一个非常专业、非常 ...",
     "title" :"公司估值- MBA智库百科",
@@ -21,24 +16,9 @@ Q_dict = {
     "title" :"一家公司的「估值」是怎么估出来的？谁来估？ - 知乎",
     },
     {
-    "url" :"https://imoney88.wordpress.com/2009/05/21/%E5%85%AC%E5%8F%B8%E4%BC%B0%E5%80%BC%E6%B3%95-company-valuation/",
-    "abstract" :"2009年5月21日 - 公司估值是指著眼於公司本身，對公司的內在價值進行評估。公司內在價值決定於公司的資產及其獲利能力。 …",
-    "title" :"公司估值法Company Valuation « 窮爸爸要看它",
-    },
-    {
-    "url" :"https://zh.wikipedia.org/zh-hk/%E8%B5%84%E4%BA%A7%E4%BC%B0%E5%80%BC",
-    "abstract" :"EV/EBITDA估值方法一般適用於資本密集、准壟斷或者具有巨額商譽的收購型公司，這樣的公司往往因為大量折舊攤銷而壓低了賬面利潤；EV/EBITDA還適用於淨利潤 ...",
-    "title" :"資產估值- 維基百科，自由的百科全書 - Wikipedia",
-    },
-    {
     "url" :"http://www.pwmchina.com/CfmlShow-1328.html",
     "abstract" :"摘要：可比交易法不对市场价值进行分析，而只是统计同类公司融资并购价格的平均溢价水平，再用这个溢价水平计算出目标公司的价值。 1、可比公司法. 首先要挑选 ...",
     "title" :"公司估值的4种计算方法你是哪一种 - 清科财富",
-    },
-    {
-    "url" :"http://big5.58cyjm.com/wenda/cyys/11788.shtml",
-    "abstract" :"創業如何估值融資融資,如何估值每個公司都有其自身價值，價值評估是資本市場參與者對一個公司在特定階段價值的判斷。非上市公司，尤其是初創公司的估值是一個 ...",
-    "title" :"創業如何估值融資- 資金創業要素 - 58創業加盟網",
     },
     {
     "url" :"https://kknews.cc/zh-hk/finance/jl852y.html",
@@ -81,51 +61,11 @@ Q_dict = {
     "title" :"公司估值问题 - Google 圖書結果",
     "metadata" :"杨峰, 林治滨 - 2012 - Business & Economics",
     },
-    {
-    "url" :"https://books.google.com.hk/books?id=CFVYlIxvP40C&pg=PA234&lpg=PA234&dq=%E4%BC%B0%E5%80%BC%EF%BC%8C%E7%8E%B0%E5%80%BC%EF%BC%8C%E4%B8%8E%E7%BB%88%E5%80%BC&source=bl&ots=vI4dotSFZw&sig=KMJeBrppZ3cQXWDOa53tWTk-5Tc&hl=zh-TW&sa=X&ved=0ahUKEwiHneaBwp_UAhWCp5QKHS5RBwQQ6AEIPjAE",
-    "abstract" :"对此,我们没有作过多的阐述,只是要懂得贴现就是将终值转换为现值的一种方法,其中后者表示的是末来支付或收取现金的公平现值 ... 9 介绍了如何用风险申性估值法来对衍生工具进行定价。 ... 0 另外一种衍生工具定价的相关方法是状态值估值法( sta ...",
-    "title" :"资本市场与公司战略(第2版):  - 第 234 頁 - Google 圖書結果",
-    "metadata" :"格林布莱特 - 2004",
-    },
-    {
-    "url" :"https://books.google.com.hk/books?id=DfdTDQAAQBAJ&pg=PT71&lpg=PT71&dq=%E4%BC%B0%E5%80%BC%EF%BC%8C%E7%8E%B0%E5%80%BC%EF%BC%8C%E4%B8%8E%E7%BB%88%E5%80%BC&source=bl&ots=zSzL_dygAD&sig=djFvpXIF0K-4-YtCndKrRV-POX0&hl=zh-TW&sa=X&ved=0ahUKEwiHneaBwp_UAhWCp5QKHS5RBwQQ6AEIQzAF",
-    "abstract" :"C ,该投资行为所产生的现金流的净现值等于 0 D 。该投资行为所产生的现金流的净现值大于 0 48 ,若 FV 代表终值, PV 代表本金(现值) , f 代表每期利率, n 代表期数, ...",
-    "title" :"证券投资分析 - Google 圖書結果",
-    "metadata" :"2011年证券业从业资格考试辅导丛书编写组编, 郁东敏 - 2011 - Business & Economics",
-    },
-    {
-    "url" :"http://realblog.zkiz.com/greatsoup38/106621",
-    "abstract" :"2014年7月18日 - htmlI黑馬：有關「投資機構如何對企業進行估值」的問題一直是企業迫切 ... 下一步， 我們就要計算終值， 通常我們用永續年金法來估算一個企業的終 ...",
-    "title" :"【案例乾貨】投資機構對企業的五種估值法（下） - 湯財文庫- 神州股票資訊 ...",
-    },
-    {
-    "url" :"https://books.google.com.hk/books?id=7NEICQkWw-4C&pg=PA97&lpg=PA97&dq=%E4%BC%B0%E5%80%BC%EF%BC%8C%E7%8E%B0%E5%80%BC%EF%BC%8C%E4%B8%8E%E7%BB%88%E5%80%BC&source=bl&ots=hIvnTDZbiV&sig=gRZjQR4E21b2_7fDqbkv-TNteSk&hl=zh-TW&sa=X&ved=0ahUKEwiHneaBwp_UAhWCp5QKHS5RBwQQ6AEITjAH",
-    "abstract" :"通常,公司终值的估计采用的是无限长期现金流折现的方式,而无限长期现金流的假定则 ... ( 4 )通过对估计的现金流进行折现,确定目标公司的折现值。 ... 公司终值的估算,选择合适的折现率对它们进行折现·经过相应的调整后看看两个估值的差别有多大。",
-    "title" :"公司并购与重组 - 第 97 頁 - Google 圖書結果",
-    "metadata" :"朱宝宪 - 2006",
-    },
-    {
-    "url" :"https://books.google.com.hk/books?id=L9FTDQAAQBAJ&pg=PT533&lpg=PT533&dq=%E4%BC%B0%E5%80%BC%EF%BC%8C%E7%8E%B0%E5%80%BC%EF%BC%8C%E4%B8%8E%E7%BB%88%E5%80%BC&source=bl&ots=vlWPbR5s4R&sig=hIYeFXC9mIQzqKzXTnMMwUrMWaA&hl=zh-TW&sa=X&ved=0ahUKEwiHneaBwp_UAhWCp5QKHS5RBwQQ6AEIUzAI",
-    "abstract" :"其DDM可用于2004年底对本公司股东权益的估值。 ... 终值的现值,应该是161 354 / 1.08955=105 110 总的估计价值的股票,则是五个股息的现值与终值现值的总和。",
-    "title" :"保险公司财务管理 - Google 圖書結果",
-    "metadata" :"陈兵, 张冬梅 - 2010 - Business & Economics",
-    },
-    {
-    "url" :"https://books.google.com.hk/books?id=l29UDQAAQBAJ&pg=PT194&lpg=PT194&dq=%E4%BC%B0%E5%80%BC%EF%BC%8C%E7%8E%B0%E5%80%BC%EF%BC%8C%E4%B8%8E%E7%BB%88%E5%80%BC&source=bl&ots=67MgXOA_gk&sig=6EaQgBeiWJw-0_nhUhak9Ps6f0U&hl=zh-TW&sa=X&ved=0ahUKEwiHneaBwp_UAhWCp5QKHS5RBwQQ6AEIWDAJ",
-    "abstract" :"【要求】理解运用预期现金流折现估值模型。 ... 未来t n 可获得现金收入A n ,则以收益率R 来评估本项目的现值,有:其中,A 1,A 2 ,... ... 根据资金“四要素”的关系,收益率由本金、终值、时间决 债券的理论价格:已知债券的预期现金流,即可对给定债券进行估值, ...",
-    "title" :"证券投资实务 - Google 圖書結果",
-    "metadata" :"王强, 陈冰 - 2009 - Business & Economics",
-    },
     ],
 ######
 #Q_1_3 #:如何彻底弄清楚一个行业或者一家公司实际上是如何经营运作的？
 ######
 "Q_1_3": ["如何彻底弄清楚一个行业或者一家公司实际上是如何经营运作的？",
-    {
-    "url" :"https://zhuanlan.zhihu.com/p/25555934",
-    "abstract" :"2017年3月4日 - 近一两个月，网络上有不少巴菲特、芒格的访谈、信件、纪录片等资源，对 ... 们交谈，彻底弄清楚一个行业或者一家公司实际上是如何经营运作的。”.",
-    "title" :"阅读推荐及学习巴菲特的正确方式- 知乎专栏",
-    },
     {
     "url" :"https://xueqiu.com/1015205532/82099261",
     "abstract" :"近一两个月，网络上有不少巴菲特、芒格的访谈、信件、纪录片等资源，对价值投资者 ... 商、客户们交谈，彻底弄清楚一个行业或者一家公司实际上是如何经营运作的。”.",
@@ -152,19 +92,9 @@ Q_dict = {
     "title" :"奇谈杂阅2017061 - 简书",
     },
     {
-    "url" :"https://www.pressreader.com/china/china-marketing/20170322/282144996208292",
-    "abstract" :"2017年3月22日 - 伯克希尔－哈撒韦公司CEO巴菲特说，彻底弄清楚一个行业或者一家公司实际上是如何经营运作的，尤其是弄明白他们的客户心里在想什么，是一个 ...",
-    "title" :"PressReader - China Marketing: 2017-03-22 - “走出去，和竞争对手 ...",
-    },
-    {
     "url" :"https://buffettmunger.blogspot.com/2017/02/cnbc_27.html",
     "abstract" :"2017年2月27日 - 巴菲特：對美國前景的樂觀判斷，是我一生投資的最大主題。 .... 供應商、客戶們交談，徹底弄清楚一個行業或者一家公司實際上是如何經營運作的。",
     "title" :"巴菲特CNBC專訪 - 巴菲特與孟格 - blogger",
-    },
-    {
-    "url" :"http://cj.sina.cn/article/detail/1655109057/177272?column=licai&ch=9",
-    "abstract" :"2017年3月2日 - 巴菲特采访全文：人生最大的投资主题是“爱国 ... 和竞争对手、供应商、客户们交谈，彻底弄清楚一个行业或者一家公司实际上是如何经营运作的。",
-    "title" :"巴菲特采访全文：人生最大的投资主题是“爱国_财经头条",
     },
     {
     "url" :"https://www.zhongba01.com/kip/essays/e1744boc.html",
@@ -185,11 +115,6 @@ Q_dict = {
     "url" :"http://wiki.mbalib.com/wiki/%E4%B8%A4%E9%98%B6%E6%AE%B5%E5%A2%9E%E9%95%BF%E6%A8%A1%E5%9E%8B",
     "abstract" :"两阶段增长模型(Two-Stage Growth Mode1)两阶段增长模型(Two Stage Growth ... 因此有必要根据行业的发展状况与公司的竞争战略地位估计公司的预测期长度。",
     "title" :"两阶段增长模型- MBA智库百科",
-    },
-    {
-    "url" :"http://wiki.mbalib.com/zh-tw/%E6%88%88%E7%99%BB%E8%82%A1%E5%88%A9%E5%A2%9E%E9%95%BF%E6%A8%A1%E5%9E%8B",
-    "abstract" :"在戈登模型中，需要預測的是下一期股利及其年增長率，而不是預計每一期的股利，以下就是固定股利增長 ... 根據這個模型，公司的股利政策會對股票價值產生影響。",
-    "title" :"戈登股利增長模型- MBA智库百科",
     },
     {
     "url" :"https://zh.wikipedia.org/zh-hk/%E8%82%A1%E6%81%AF%E8%B2%BC%E7%8F%BE%E6%A8%A1%E5%9E%8B",
@@ -213,17 +138,6 @@ Q_dict = {
     "metadata" :"王少豪 - 2002 - Business enterprises",
     },
     {
-    "url" :"https://books.google.com.hk/books?id=CFVYlIxvP40C&pg=PA376&lpg=PA376&dq=%E5%85%AC%E5%8F%B8%E5%A2%9E%E9%95%BF+%E6%A8%A1%E5%9E%8B%E9%A2%84%E6%B5%8B&source=bl&ots=vI4dotSGSt&sig=sSyRFDGADqARn3H_JQL3C-ps42Q&hl=zh-TW&sa=X&ved=0ahUKEwjsssKOwp_UAhWFFpQKHaHgDD8Q6AEIWjAH",
-    "abstract" :"股利贴现模型。有时候财务分析人员在预测项目收益率和未来收人时会采用股利贴现模型(我们已在第 9 章介绍过了)的一个特殊形式。这个模型叫戈登增长模型( ...",
-    "title" :"资本市场与公司战略(第2版):  - 第 376 頁 - Google 圖書結果",
-    "metadata" :"格林布莱特 - 2004",
-    },
-    {
-    "url" :"http://baike.baidu.com/item/%E6%88%88%E7%99%BB%E8%82%A1%E5%88%A9%E5%A2%9E%E9%95%BF%E6%A8%A1%E5%9E%8B",
-    "abstract" :"戈登股利增长模型又称为“股利贴息不变增长模型”、“戈登模型(Gordon Model)”，在大 ... 该模型通过计算公司预期未来支付给股东的股利现值，来确定股票的内在价值，它 ... 在戈登模型中，需要预测的是下一期股利及其年增长率，而不是预计每一期的 ...",
-    "title" :"戈登股利增长模型_百度百科",
-    },
-    {
     "url" :"https://books.google.com.hk/books?id=4HX8URZj-3kC&pg=PA129&lpg=PA129&dq=%E5%85%AC%E5%8F%B8%E5%A2%9E%E9%95%BF+%E6%A8%A1%E5%9E%8B%E9%A2%84%E6%B5%8B&source=bl&ots=XRcVAyIYjn&sig=CRv_A42tvwqIH2b3vvkz1zuOghY&hl=zh-TW&sa=X&ved=0ahUKEwjsssKOwp_UAhWFFpQKHaHgDD8Q6AEIZjAJ",
     "abstract" :"红利增长模型适用于成长型公司股票评估。成长型公司具有 ... 运用红利增长模型时,对增长率 9 的预测要谨慎,因为增长率对股票价格的影响十分明显。股利增长率母的 ...",
     "title" :"资产评估 - 第 129 頁 - Google 圖書結果",
@@ -232,33 +146,19 @@ Q_dict = {
     ],
 }
 keyword_list = [ 
-   "   网络",
-   "   可视化",
-   "   本质",
-   "   计算复杂度:常，对，线，平，立，指，阶",
-   "   意义",
-   "   费米计算",
-   "   演化",
-   "   从心开始",
-   "   验证",
-   "   选择",
-   "   trade off",
-   "   外延",
-   "   合并",
-   "   生老病死",
-   "   推动",
-   "   诗歌",
-   "   奇点",
-   "   未来",
-   "   不确定",
-   "   高维",
-   "   随机",
-   "   分类",
-   "   抽象",
-   "   很感谢你能能来，不遗憾你离开",
-   "   时间",
+   "相对估值法",
+   "PE 估值法",
+   "PB 估值法",
+   "PEG 估值法",   
+   "PS 估值法",
+   "EV/EBITDA 估值法",
+   "绝对估值折现法  估值法",
+   "现金流量折现 估值法",
+   "PS 估值法",
+   "公司的财物模型",
+   "公司的构成模型",
 ]
-meta_data = {"status":"hc","Major": 1,"Minor": 4,"Prefix": "Q"}
+meta_data = {"status":"mc","Major": 1,"Minor": 4,"Prefix": "Q"}
 km_0_title = "公司估值"
 km_0_summary = "ummary_tp"
 km_1_tags = []
