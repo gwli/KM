091d2db1b06d2b56bbfed15d97eaa14f1d319b49
victor <gwli@outlook.com>
2017-06-05 23:29:52 +0800

    战争

---
+++ source/GongXiang/ChongDianBao.py
@@ -0,0 +1,20 @@
+
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   " 充电宝的费用",
+   " 共享充电宝，可以解决实时不断电的问题",
+   " anytime,anywhere,anyway  本质",
+   " 共享充电宝 盈利模式 ",
+   " 充电宝的价格",
+   " 太阳能充电的效率",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "共享充电宝"
+km_0_summary = "ummary_tp"
+km_1_tags = []
