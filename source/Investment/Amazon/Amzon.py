d74ba0d9684b583020bd058b251d869955c734c6
victor <gwli@outlook.com>
2017-06-08 10:02:20 +0800

    更新比特币的认识

---
+++ source/Investment/Amazon/Amzon.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "亚马逊 股价 通过 1000的理由",
+   "还有些公司 股价 突破 1000$",
+   "未来哪些公司的股价可能突破1000$",
+   "未来出现可能出现6万亿平台会是什么",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "亚马逊的股价"
+km_0_summary = "亚马逊"
+km_1_tags = []
