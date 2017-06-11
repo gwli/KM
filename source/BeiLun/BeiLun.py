bb01dd3dd14c25c544358726669bd589aff06cd3
victor <gwli@outlook.com>
2017-06-11 21:39:00 +0800

    悖论与批判

---
+++ source/BeiLun/BeiLun.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+"如何快速的寻找悖论",
+"穿的比较少的女孩是怎么样想的",
+"头发发型与性格的关系",
+"服装与性格",
+"黑帮大老，周边的人是如何选的",
+"人生都有哪些难处",
+"心不在焉，有什么样的进化价值？",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "悖论"
+km_0_summary = "ummary_tp"
+km_1_tags = []
