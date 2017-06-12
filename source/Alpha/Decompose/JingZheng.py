57056db5a7757e546b5d89b323cbb3d0429e9695
victor <gwli@outlook.com>
2017-06-12 09:37:51 +0800

    分工

---
+++ source/Alpha/Decompose/JingZheng.py
@@ -0,0 +1,15 @@
+
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+"博异伦中一报还一报策略最稳定最终会有一种平衡策略，这只是一种平衡",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "竟争"
+km_0_summary = "ummary_tp"
+km_1_tags = []
