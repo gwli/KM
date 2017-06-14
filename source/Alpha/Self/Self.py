4e0e1f7f81d5d68dbddaaa2e349a81b048cceb3f
victor <gwli@outlook.com>
2017-06-14 23:07:35 +0800

    本我

---
+++ source/Alpha/Self/Self.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "二元论 是指什么",
+   "唯我论 是什么",
+   "唯我论 有意义",
+   "斯宾诺莎的世界观是什么",
+]
+meta_data = {"status":".","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "self"
+km_0_summary = "ummary_tp"
+km_1_tags = []
