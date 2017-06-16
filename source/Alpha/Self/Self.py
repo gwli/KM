d090b899a2320160f5c95541b735c35475b91bf1
victor <gwli@outlook.com>
2017-06-16 08:34:27 +0800

    思想-> 模型

--- source/Alpha/Self/Self.py
+++ source/Alpha/Self/Self.py
@@ -11,8 +11,10 @@ keyword_list = [
    "唯我论 有意义",
    "斯宾诺莎的世界观是什么",
    "感情是什么，也是一种生化算法",
+   "如何把思考模型代码吗",
+   "如何度量各种主义之间的距离",
 ]
-meta_data = {"status":".","Major": 0,"Minor": 0,"Prefix": "Q"}
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
 km_0_title = "self"
 km_0_summary = "ummary_tp"
 km_1_tags = []
