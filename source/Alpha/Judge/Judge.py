c7add77fc839a10560ac37226a7825afde61a699
victor <gwli@outlook.com>
2017-06-18 20:12:29 +0800

    Judge

---
+++ source/Alpha/Judge/Judge.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "最新思想模型从哪里，论文",
+   "全球每年出版多少本书",
+]
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "策略"
+km_0_summary = "ummary_tp"
+km_1_tags = []
+
