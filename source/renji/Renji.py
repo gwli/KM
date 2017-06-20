598adfbc07a09b64395d43f88c6f241053948022
victor <gwli@outlook.com>
2017-06-20 22:14:26 +0800

    故事

--- source/renji/Renji.py
+++ source/renji/Renji.py
@@ -1,17 +1,19 @@
 # -*- coding:utf-8 -*- 
-
 Q_dict = { 
 ######
 #Q_0_0 #:adfadfafa: : : : 
 ######
 }
+
 keyword_list = [ 
    "什么是成就需要低的人",
    "对图解读更乐观，更偏向于事业和正能量的一般成需要就比较高",
    "如何储备人际价值，到人际价值高的地方扎堆，TED的现场",
    "如何查看别人的微信朋友圈",
+   "人际关系的六度空间",
 ]
-meta_data = {"status":".","Major": 0,"Minor": 0,"Prefix": "Q"}
+
+meta_data = {"status":"mc","Major": 0,"Minor": 0,"Prefix": "Q"}
 km_0_title = "title_tp"
 km_0_summary = "ummary_tp"
 km_1_tags = []
