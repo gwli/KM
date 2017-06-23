9f3f6cdeda849ff130459320f8d55fc511b90c01
victor <gwli@outlook.com>
2017-06-23 21:53:00 +0800

    创新

---
+++ source/ChuangXin/ChangXin.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "创新为什么这么难",
+   "如何创新",
+   "为什么大企业的创新也很难",
+   "IBM 到现在往上走，还是往下走",
+   "为什么大家害怕创新",
+   "为什么大部分创新都是收购过来的",
+]
+meta_data = {"status":"hc","Major": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "创新"
+km_0_summary = "ummary_tp"
+km_1_tags = []
