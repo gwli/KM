1ea1974f229cd020cf2ff5445dbe9e3fd2f205d9
victor <gwli@outlook.com>
2017-07-05 11:17:15 +0800

    比特币

---
+++ source/Alpha/ShiGe/ShiGe.py
@@ -0,0 +1,15 @@
+
+# -*- coding:utf-8 -*- 
+
+Q_dict = { 
+######
+#Q_0_0 #:adfadfafa: : : : 
+######
+}
+keyword_list = [ 
+   "为什么诗家与远方是大家梦想",
+]
+meta_data = {"status":"hc","诗歌": 0,"Minor": 0,"Prefix": "Q"}
+km_0_title = "诗歌"
+km_0_summary = "ummary_tp"
+km_1_tags = []
